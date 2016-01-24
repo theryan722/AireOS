804a9435071559baf666eabcdd189025c2ff0b7f
Ryan O'Day <romanguy722@gmail.com>
2016-01-23 22:13:42 -0500

    Aire.API.Power Skeleton

---
+++ src/Aire.API/Power.vb
@@ -0,0 +1,21 @@
+﻿Namespace System
+    Public Class Power
+
+        Public Shared Sub Shutdown()
+
+        End Sub
+
+        Public Shared Sub Restart()
+
+        End Sub
+
+        Public Shared Sub Hibernate()
+
+        End Sub
+
+        Public Shared Sub Sleep()
+
+        End Sub
+
+    End Class
+End Namespace
