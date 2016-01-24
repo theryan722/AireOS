e61fa04fc31f9ace7d682f134f2c3635b0f75c18
Ryan O'Day <romanguy722@gmail.com>
2016-01-24 13:54:39 -0500

    Outline dlgLogout

--- src/Aire.API/Dialogs/dlgLogout.vb
+++ src/Aire.API/Dialogs/dlgLogout.vb
@@ -1,3 +1,28 @@
 ﻿Friend Class dlgLogout
 
+#Region "UI"
+
+    Private Sub btnLogout_Click(sender As Object, e As EventArgs) Handles btnLogout.Click
+
+    End Sub
+
+    Private Sub btnSwitchUser_Click(sender As Object, e As EventArgs) Handles btnSwitchUser.Click
+
+    End Sub
+
+    Private Sub btnLock_Click(sender As Object, e As EventArgs) Handles btnLock.Click
+
+    End Sub
+
+#End Region
+
+#Region "dlgLogout"
+
+    Private Sub dlgLogout_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+#End Region
+    
+
 End Class
