37f1f53eb8a5da6203fa819e1421cfa7524d32ab
Ryan O'Day <romanguy722@gmail.com>
2016-01-23 22:56:30 -0500

    Close frmPower after button press

--- src/PowerDialog/frmPower.vb
+++ src/PowerDialog/frmPower.vb
@@ -4,18 +4,22 @@
 
     Private Sub btnShutdown_Click(sender As Object, e As EventArgs) Handles btnShutdown.Click
         Aire.API.System.Power.Shutdown()
+        Me.Close()
     End Sub
 
     Private Sub btnSleep_Click(sender As Object, e As EventArgs) Handles btnSleep.Click
         Aire.API.System.Power.Sleep()
+        Me.Close()
     End Sub
 
     Private Sub btnRestart_Click(sender As Object, e As EventArgs) Handles btnRestart.Click
         Aire.API.System.Power.Restart()
+        Me.Close()
     End Sub
 
     Private Sub btnHibernate_Click(sender As Object, e As EventArgs) Handles btnHibernate.Click
         Aire.API.System.Power.Hibernate()
+        Me.Close()
     End Sub
 
 #End Region
