071164f78cb57159bcb6639233ad004e3b6ca48d
Ryan O'Day <romanguy722@gmail.com>
2016-01-23 22:21:40 -0500

    Handle btnShutdown Click()

--- src/PowerDialog/frmPower.vb
+++ src/PowerDialog/frmPower.vb
@@ -1,3 +1,7 @@
 ﻿Public Class frmPower
 
+    Private Sub btnShutdown_Click(sender As Object, e As EventArgs) Handles btnShutdown.Click
+        Aire.API.System.Power.Shutdown()
+    End Sub
+
 End Class
