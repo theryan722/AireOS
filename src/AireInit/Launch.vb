6b08eea020cd9194106ba7b30198727c69e256d9
Ryan O'Day <romanguy722@gmail.com>
2016-01-23 21:33:21 -0500

    Tidy up AireInit

--- src/AireInit/Launch.vb
+++ src/AireInit/Launch.vb
@@ -1,10 +1,8 @@
 ﻿Module Launch
 
-
     'Initializes Aire OS
     Sub Main()
         Console.Title = "Aire OS Initialization"
-        'initialization code here
         Aire.Login.Manager.DisplayInitialLogin()
     End Sub
 
