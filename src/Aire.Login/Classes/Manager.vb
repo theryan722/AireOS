ab2486f25007b062dddac1546ad98ff8703997ec
Ryan O'Day <romanguy722@gmail.com>
2016-01-23 21:35:44 -0500

    Implement Aire.Login.Manager.DisplayInitialLogin()

--- src/Aire.Login/Classes/Manager.vb
+++ src/Aire.Login/Classes/Manager.vb
@@ -1,7 +1,10 @@
 ﻿Public Class Manager
 
     Public Shared Sub DisplayInitialLogin()
-
+        Dim newb As New frmLoginBackground
+        Dim newl As New frmLogin
+        newb.Show()
+        newl.ShowDialog(newb)
     End Sub
 
 End Class
