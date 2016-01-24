23d8f6737c13a3195a8e75fd22273b3b87d426e4
Ryan O'Day <romanguy722@gmail.com>
2016-01-24 00:57:17 -0500

    Further outline Users API

---
+++ src/Aire.API/Users.vb
@@ -0,0 +1,43 @@
+﻿Namespace User
+
+    Namespace Current
+
+        Public Class Info
+
+            Public Shared Function GetUsername() As String
+
+            End Function
+
+            Public Shared Function MatchPassword(ByVal pass As String) As Boolean
+
+            End Function
+
+            Public Shared Function GetPermissionLevel() As Integer
+
+            End Function
+
+            Public Shared Function GetDataStorageLocation() As String
+
+            End Function
+
+        End Class
+
+    End Namespace
+
+    Namespace Users
+
+        Public Class Info
+
+            Public Shared Function CheckIfUserExists(ByVal username As String) As Boolean
+
+            End Function
+
+            Public Shared Function GetPermissionLevelForUser(ByVal username As String) As Integer
+
+            End Function
+
+        End Class
+
+    End Namespace
+
+End Namespace
--- src/Aire.Login/Forms/frmLogin.vb
+++ src/Aire.Login/Forms/frmLogin.vb
@@ -62,4 +62,7 @@
 
 #End Region
 
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+
+    End Sub
 End Class
