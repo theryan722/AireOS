1abe00cc64d9ee09c04afa96c37952220ac43656
Ryan O'Day <romanguy722@gmail.com>
2016-01-24 13:30:26 -0500

    Outline of repair api

---
+++ src/Aire.DataManager/Repair.vb
@@ -0,0 +1,23 @@
+﻿Public Class Repair
+
+#Region "Check"
+
+    Public Shared Sub CheckAllData()
+
+
+
+    End Sub
+
+#End Region
+
+#Region "Repair"
+
+    Public Shared Sub RepairAllData()
+
+
+
+    End Sub
+
+#End Region
+
+End Class
