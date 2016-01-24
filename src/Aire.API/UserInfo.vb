d8e660ddec2349e10b31ad91c5636a9d62052270
Ryan O'Day <romanguy722@gmail.com>
2016-01-24 00:28:17 -0500

    Skeleton of  Aire.API.Info

---
+++ src/Aire.API/UserInfo.vb
@@ -0,0 +1,23 @@
+﻿Namespace User
+
+    Namespace Current
+
+        Public Class Info
+
+            Public Shared Function GetUsername() As String
+
+            End Function
+
+            Public Shared Function MatchPassword(ByVal pass As String) As Boolean
+
+            End Function
+
+            Public Shared Function GetPermissionLevel() As Integer
+
+            End Function
+
+        End Class
+
+    End Namespace
+
+End Namespace
