926918b78915ea8c243490ce665725f92b2f2fe6
Ryan O'Day <romanguy722@gmail.com>
2016-01-23 22:21:55 -0500

    Handle btnSleep Click()

--- src/PowerDialog/frmPower.vb
+++ src/PowerDialog/frmPower.vb
@@ -4,4 +4,8 @@
         Aire.API.System.Power.Shutdown()
     End Sub
 
+    Private Sub btnSleep_Click(sender As Object, e As EventArgs) Handles btnSleep.Click
+        Aire.API.System.Power.Sleep()
+    End Sub
+
 End Class
