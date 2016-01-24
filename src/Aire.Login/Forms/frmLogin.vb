fa846744991d73692cba08802938e4ecad79c08f
Ryan O'Day <romanguy722@gmail.com>
2016-01-23 21:36:28 -0500

    Regions for frmLogin

--- src/Aire.Login/Forms/frmLogin.vb
+++ src/Aire.Login/Forms/frmLogin.vb
@@ -1,3 +1,24 @@
 ﻿Public Class frmLogin
 
+#Region "UI"
+
+
+
+#End Region
+
+#Region "Methods"
+
+
+
+#End Region
+
+#Region "frmLogin"
+
+
+
+#End Region
+
+    Private Sub frmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
