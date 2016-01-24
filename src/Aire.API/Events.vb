c1bb351da86a56ec69706c8f688dce74faae1457
Ryan O'Day <romanguy722@gmail.com>
2016-01-23 22:32:53 -0500

    Aire.API.Events class, and new event SystemPowerStateChange

---
+++ src/Aire.API/Events.vb
@@ -0,0 +1,14 @@
+﻿Namespace System
+
+    Public Enum PowerState
+        Shutdown
+        Restart
+        Hibernate
+        Sleep
+    End Enum
+
+    Public Class Events
+        Public Shared Event SystemPowerStateChange(ByVal change As PowerState)
+    End Class
+
+End Namespace
