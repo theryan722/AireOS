ccdc5ab087212f96f58c98ac75d0fa012c83d057
Ryan O'Day <romanguy722@gmail.com>
2016-01-23 22:38:19 -0500

    Add regions to Events class, add Raise_SystemPowerStateChange()

--- src/Aire.API/Events.vb
+++ src/Aire.API/Events.vb
@@ -8,7 +8,21 @@
     End Enum
 
     Public Class Events
+
+#Region "Events"
+
         Public Shared Event SystemPowerStateChange(ByVal change As PowerState)
+
+#End Region
+
+#Region "Raise Event Methods"
+
+        Public Shared Sub Raise_SystemPowerStateChange(ByVal change As PowerState)
+            RaiseEvent SystemPowerStateChange(change)
+        End Sub
+
+#End Region
+
     End Class
 
 End Namespace
