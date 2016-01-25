4b68d61351b12af4c4812e0f9387b126f07023c0
Ryan O'Day <romanguy722@gmail.com>
2016-01-24 21:18:44 -0500

    ModifySalt()

--- src/Aire.DataManager/DBAccess.vb
+++ src/Aire.DataManager/DBAccess.vb
@@ -31,6 +31,10 @@ Namespace Databases
             DBConnection.Update(username, """PASSWORD""", password)
         End Sub
 
+        Public Shared Sub ModifySalt(ByVal username As String, ByVal salt As String)
+            DBConnection.Update(username, "SALT", salt)
+        End Sub
+
 #End Region
 
     End Class
