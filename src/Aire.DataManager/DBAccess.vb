77994d86e585c3ddbef5ab3c036987419414da2f
Ryan O'Day <romanguy722@gmail.com>
2016-01-24 21:41:42 -0500

    Implement GetPasswordHint()

--- src/Aire.DataManager/DBAccess.vb
+++ src/Aire.DataManager/DBAccess.vb
@@ -112,7 +112,16 @@ Namespace Databases
         End Function
 
         Public Shared Function GetPasswordHint(ByVal username As String) As String
-
+            Try
+                Dim result As String
+                Dim query As [String] = "SELECT PASSWORDHINT FROM USERS WHERE USERNAME = '" & username & "';"
+                result = DBConnection.ExecuteScalar(query)
+                Return result
+            Catch fail As Exception
+                Dim [error] As [String] = "The following error has occurred:" & vbLf & vbLf
+                [error] += fail.Message.ToString() + vbLf & vbLf
+                MessageBox.Show([error])
+            End Try
         End Function
 
         Public Shared Function GetPermissionLevel(ByVal username As String) As String
