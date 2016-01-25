e30dcc4c0baac1f8bb5861852a360a9ef76f56a8
Ryan O'Day <romanguy722@gmail.com>
2016-01-24 22:58:26 -0500

    Aire.APi.Cryptography Namespace

---
+++ src/Aire.API/Passwords.vb
@@ -0,0 +1,21 @@
+﻿Imports BCrypt.Net.BCrypt
+
+Namespace Cryptography
+
+    Public Class Passwords
+
+        Public Shared Function HashPassword(ByVal pass As String, ByVal salt As String) As String
+            Return HashPassword(pass, salt)
+        End Function
+
+        Public Shared Function GenerateSalt() As String
+            Return GenerateSalt()
+        End Function
+
+        Public Shared Function CheckPassword(ByVal pass As String, ByVal hashedpass As String) As Boolean
+            Return BCrypt.Net.BCrypt.Verify(pass, hashedpass)
+        End Function
+
+    End Class
+
+End Namespace
