90a863ed9d525be89230c198bf3fc1001bdf94a3
Ryan O'Day <romanguy722@gmail.com>
2016-01-26 17:59:43 -0500

    Outline of Audio volume API

---
+++ src/Aire.API/Volume.vb
@@ -0,0 +1,31 @@
+﻿Namespace Audio
+
+    Public Class Volume
+
+        Public Shared Sub Increase(ByVal amount As Integer)
+
+        End Sub
+
+        Public Shared Sub Decrease(ByVal amount As Integer)
+
+        End Sub
+
+        Public Shared Sub Mute()
+
+        End Sub
+
+        Public Shared Sub Unmute()
+
+        End Sub
+
+        Public Shared Sub ToggleMute()
+
+        End Sub
+
+        Public Shared Function GetVolume() As Integer
+
+        End Function
+
+    End Class
+
+End Namespace
