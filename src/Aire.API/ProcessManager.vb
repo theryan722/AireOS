73da497b25122f711c754c2c458f6eeff9098729
Ryan O'Day <romanguy722@gmail.com>
2016-01-26 18:15:38 -0500

    Play try catch blocks in process api, organize with regions

--- src/Aire.API/ProcessManager.vb
+++ src/Aire.API/ProcessManager.vb
@@ -4,6 +4,8 @@
         System.Diagnostics.Process.Start(proc)
     End Sub
 
+#Region "ExecuteCommand"
+
     Public Shared Sub ExecuteCommand(ByVal file As String, ByVal arguments As String)
         Try
             Dim proc As System.Diagnostics.Process = New System.Diagnostics.Process()
@@ -14,43 +16,63 @@
             proc.StartInfo.CreateNoWindow = True
             proc.Start()
         Catch ex As Exception
-            Sys.Logging.Log.Write("")
+            Sys.Logging.Log.Write("ERROR: " & ex.ToString)
         End Try
     End Sub
 
     Public Shared Function ExecuteCommandWithOutput(ByVal file As String, ByVal arguments As String) As String
-        Dim ret As String = ""
-        Dim proc As New System.Diagnostics.Process()
-        proc.StartInfo.FileName = file
-        proc.StartInfo.Arguments = arguments
-        proc.StartInfo.UseShellExecute = False
-        proc.StartInfo.RedirectStandardOutput = True
-        proc.Start()
-        ret = proc.StandardOutput.ReadToEnd()
-        proc.WaitForExit()
-        Return ret
+        Try
+            Dim ret As String = ""
+            Dim proc As New System.Diagnostics.Process()
+            proc.StartInfo.FileName = file
+            proc.StartInfo.Arguments = arguments
+            proc.StartInfo.UseShellExecute = False
+            proc.StartInfo.RedirectStandardOutput = True
+            proc.Start()
+            ret = proc.StandardOutput.ReadToEnd()
+            proc.WaitForExit()
+            Return ret
+        Catch ex As Exception
+            Sys.Logging.Log.Write("ERROR: " & ex.ToString)
+            Return Nothing
+        End Try
     End Function
 
+#End Region
+
+#Region "ExecuteScript"
+
     Public Shared Sub ExecuteScript(ByVal script As String)
-        Dim proc As New System.Diagnostics.Process()
-        proc.StartInfo.FileName = "/bin/bash"
-        proc.StartInfo.Arguments = script
-        proc.StartInfo.UseShellExecute = False
-        proc.StartInfo.RedirectStandardOutput = True
-        proc.Start()
+        Try
+            Dim proc As New System.Diagnostics.Process()
+            proc.StartInfo.FileName = "/bin/bash"
+            proc.StartInfo.Arguments = script
+            proc.StartInfo.UseShellExecute = False
+            proc.StartInfo.RedirectStandardOutput = True
+            proc.Start()
+        Catch ex As Exception
+            Sys.Logging.Log.Write("ERROR: " & ex.ToString)
+        End Try
     End Sub
 
     Public Shared Function ExecuteScriptWithOutput(ByVal script As String)
-        Dim ret As String = ""
-        Dim proc As New System.Diagnostics.Process()
-        proc.StartInfo.FileName = "/bin/bash"
-        proc.StartInfo.Arguments = script
-        proc.StartInfo.UseShellExecute = False
-        proc.StartInfo.RedirectStandardOutput = True
-        proc.Start()
-        ret = proc.StandardOutput.ReadToEnd()
-        proc.WaitForExit()
-        Return ret
+        Try
+            Dim ret As String = ""
+            Dim proc As New System.Diagnostics.Process()
+            proc.StartInfo.FileName = "/bin/bash"
+            proc.StartInfo.Arguments = script
+            proc.StartInfo.UseShellExecute = False
+            proc.StartInfo.RedirectStandardOutput = True
+            proc.Start()
+            ret = proc.StandardOutput.ReadToEnd()
+            proc.WaitForExit()
+            Return ret
+        Catch ex As Exception
+            Sys.Logging.Log.Write("ERROR: " & ex.ToString)
+            Return Nothing
+        End Try
     End Function
+
+#End Region
 
 End Class
