983c6e99b467be2971fefd754b9d114b08d2aa2a
Ryan O'Day <romanguy722@gmail.com>
2016-01-25 20:44:14 -0500

    Portion of File Dialog UI

--- src/Aire.API/Dialogs/dlgFile.Designer.vb
+++ src/Aire.API/Dialogs/dlgFile.Designer.vb
@@ -22,8 +22,146 @@ Partial Class dlgFile
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.pnl_bottom = New System.Windows.Forms.Panel()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.btnOpenSave = New System.Windows.Forms.Button()
+        Me.combo_fileextensions = New System.Windows.Forms.ComboBox()
+        Me.txt_file = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.pnl_top = New System.Windows.Forms.Panel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.pnl_bottom.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnl_bottom
+        '
+        Me.pnl_bottom.Controls.Add(Me.Panel3)
+        Me.pnl_bottom.Controls.Add(Me.Panel1)
+        Me.pnl_bottom.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.pnl_bottom.Location = New System.Drawing.Point(0, 284)
+        Me.pnl_bottom.Name = "pnl_bottom"
+        Me.pnl_bottom.Size = New System.Drawing.Size(489, 45)
+        Me.pnl_bottom.TabIndex = 0
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Dock = System.Windows.Forms.DockStyle.Right
+        Me.btnCancel.Location = New System.Drawing.Point(76, 0)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancel.TabIndex = 0
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'btnOpenSave
+        '
+        Me.btnOpenSave.Dock = System.Windows.Forms.DockStyle.Right
+        Me.btnOpenSave.Location = New System.Drawing.Point(2, 0)
+        Me.btnOpenSave.Name = "btnOpenSave"
+        Me.btnOpenSave.Size = New System.Drawing.Size(74, 23)
+        Me.btnOpenSave.TabIndex = 1
+        Me.btnOpenSave.Text = "Open"
+        Me.btnOpenSave.UseVisualStyleBackColor = True
+        '
+        'combo_fileextensions
+        '
+        Me.combo_fileextensions.Dock = System.Windows.Forms.DockStyle.Top
+        Me.combo_fileextensions.FormattingEnabled = True
+        Me.combo_fileextensions.Location = New System.Drawing.Point(0, 0)
+        Me.combo_fileextensions.Name = "combo_fileextensions"
+        Me.combo_fileextensions.Size = New System.Drawing.Size(151, 21)
+        Me.combo_fileextensions.TabIndex = 2
+        '
+        'txt_file
+        '
+        Me.txt_file.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txt_file.Location = New System.Drawing.Point(57, 0)
+        Me.txt_file.Name = "txt_file"
+        Me.txt_file.Size = New System.Drawing.Size(281, 20)
+        Me.txt_file.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "File Name:"
+        '
+        'pnl_top
+        '
+        Me.pnl_top.Dock = System.Windows.Forms.DockStyle.Top
+        Me.pnl_top.Location = New System.Drawing.Point(0, 0)
+        Me.pnl_top.Name = "pnl_top"
+        Me.pnl_top.Size = New System.Drawing.Size(489, 40)
+        Me.pnl_top.TabIndex = 1
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Controls.Add(Me.combo_fileextensions)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Panel1.Location = New System.Drawing.Point(338, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(151, 45)
+        Me.Panel1.TabIndex = 5
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.btnOpenSave)
+        Me.Panel2.Controls.Add(Me.btnCancel)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel2.Location = New System.Drawing.Point(0, 22)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(151, 23)
+        Me.Panel2.TabIndex = 3
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.txt_file)
+        Me.Panel3.Controls.Add(Me.Label1)
+        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel3.Location = New System.Drawing.Point(0, 0)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(338, 20)
+        Me.Panel3.TabIndex = 6
+        '
+        'dlgFile
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "dlgFile"
+        Me.ClientSize = New System.Drawing.Size(489, 329)
+        Me.Controls.Add(Me.pnl_top)
+        Me.Controls.Add(Me.pnl_bottom)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "dlgFile"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = "File"
+        Me.pnl_bottom.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents pnl_bottom As System.Windows.Forms.Panel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txt_file As System.Windows.Forms.TextBox
+    Friend WithEvents combo_fileextensions As System.Windows.Forms.ComboBox
+    Friend WithEvents btnOpenSave As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+    Friend WithEvents pnl_top As System.Windows.Forms.Panel
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents Panel3 As System.Windows.Forms.Panel
 End Class
