78b88294547dcf05605cadf51c0eba6b23503de2
Ryan O'Day <romanguy722@gmail.com>
2016-01-26 16:56:28 -0500

    dlgFolder outline

--- src/Aire.API/Dialogs/dlgFolder.vb
+++ src/Aire.API/Dialogs/dlgFolder.vb
@@ -1,3 +1,17 @@
-﻿Friend Class dlgFolder
+﻿Public Class dlgFolder
+
+#Region "Methods"
+
+
+
+#End Region
+
+#Region "dlgFolder"
+
+    Private Sub dlgFolder_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+#End Region
 
 End Class
