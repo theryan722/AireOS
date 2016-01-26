77d3df93800ac7d9792121f2cf2487c86fd132c3
Ryan O'Day <romanguy722@gmail.com>
2016-01-26 18:00:38 -0500

    Outline of ExecuteCommand

--- src/Aire.API/ProcessManager.vb
+++ src/Aire.API/ProcessManager.vb
@@ -4,4 +4,12 @@
         System.Diagnostics.Process.Start(proc)
     End Sub
 
+    Public Shared Sub ExecuteCommand(ByVal file As String, ByVal arguments As String)
+
+    End Sub
+
+    Public Shared Function ExecuteCommandWithOutput(ByVal file As String, ByVal arguments As String) As String
+
+    End Function
+
 End Class
