dd0a62cb03545ee805c70fc3eff7bcb67ab4ebb8
Ryan O'Day <romanguy722@gmail.com>
2016-01-26 16:57:00 -0500

    dlgFile outline

--- src/Aire.API/Dialogs/dlgFile.vb
+++ src/Aire.API/Dialogs/dlgFile.vb
@@ -1,3 +1,17 @@
-﻿Friend Class dlgFile
+﻿Public Class dlgFile
+
+#Region "Methods"
+
+
+
+#End Region
+
+#Region "dlgFile"
+
+    Private Sub dlgFile_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+#End Region
 
 End Class
