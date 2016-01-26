d1743e759e14a3f3fccb79703beb9648b6996b34
Ryan O'Day <romanguy722@gmail.com>
2016-01-26 16:57:51 -0500

    UI Region

--- src/Aire.API/Dialogs/dlgFolder.vb
+++ src/Aire.API/Dialogs/dlgFolder.vb
@@ -14,4 +14,17 @@
 
 #End Region
 
+#Region "UI"
+
+    Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+
+    End Sub
+
+#End Region
+
+    
 End Class
