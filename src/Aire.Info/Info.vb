2a40880235e8a6c63fcca722a1d9259ea9c49075
Ryan O'Day <romanguy722@gmail.com>
2016-01-27 16:26:49 -0500

    Aire.Info library, outline info class

---
+++ src/Aire.Info/Info.vb
@@ -0,0 +1,19 @@
+﻿Public Class Info
+
+    Public Shared Function OSName() As String
+
+    End Function
+
+    Public Shared Function OSVersion() As String
+
+    End Function
+
+    Public Shared Function OSRelease() As String
+
+    End Function
+
+    Public Shared Function Website() As String
+
+    End Function
+
+End Class
