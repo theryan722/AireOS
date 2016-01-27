63fe79b3b95a047d6405a58ada76149b3b74ff1b
Ryan O'Day <romanguy722@gmail.com>
2016-01-27 18:09:14 -0500

    frmLauncher, outline of frmLauncher code

---
+++ src/Aire.Desktop/Forms/frmLauncher.vb
@@ -0,0 +1,33 @@
+﻿Public Class frmLauncher
+
+#Region "Variables/Properties"
+
+    Private bar As bar_launchbar
+    Private desktop As frmDesktop
+    Public Shared Property Showing As Boolean = False
+
+#End Region
+
+#Region "UI"
+
+
+
+#End Region
+
+#Region "Methods"
+
+
+
+#End Region
+
+#Region "bar_appbar"
+
+    Public Sub New(ByVal frm As bar_launchbar, ByVal frm2 As frmDesktop)
+        InitializeComponent()
+        bar = frm
+        desktop = frm2
+    End Sub
+
+#End Region
+
+End Class
