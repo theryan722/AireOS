d9dedd4879edbf2067c9f41e04539670ac9e8213
Ryan O'Day <romanguy722@gmail.com>
2016-01-27 17:32:04 -0500

    DesktopWindows class (changing original concept of how multiple desktops would be managed)

---
+++ src/Aire.Desktop/Classes/DesktopWindows.vb
@@ -0,0 +1,9 @@
+﻿Public Class DesktopWindows
+
+    Public Property Windows As List(Of Process) 'Unsure of how will be storing window info, use process for now as placeholder
+
+    Public Sub New(ByVal frm As frmDesktop)
+
+    End Sub
+
+End Class
--- src/Aire.Desktop/Forms/frmDesktop.vb
+++ src/Aire.Desktop/Forms/frmDesktop.vb
@@ -1,3 +1,27 @@
 ﻿Public Class frmDesktop
 
+#Region "Properties"
+
+
+
+#End Region
+
+#Region "Methods"
+
+
+
+#End Region
+
+#Region "UI"
+
+
+
+#End Region
+
+#Region "frmDesktop"
+
+
+
+#End Region
+
 End Class
