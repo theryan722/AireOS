a032448dd3d9bd528deeea16538a50f2ed4499a1
Ryan O'Day <romanguy722@gmail.com>
2016-01-27 16:28:04 -0500

    Implement info class

--- src/Aire.Info/Info.vb
+++ src/Aire.Info/Info.vb
@@ -1,19 +1,19 @@
 ﻿Public Class Info
 
     Public Shared Function OSName() As String
-
+        Return "Aire OS"
     End Function
 
     Public Shared Function OSVersion() As String
-
+        Return "0.0.0.1"
     End Function
 
     Public Shared Function OSRelease() As String
-
+        Return "Official|Pre-Alpha"
     End Function
 
     Public Shared Function Website() As String
-
+        Return "https://github.com/theryan722/aireos"
     End Function
 
 End Class
