6445a3e8f1e14b78636c3f6c083cd5a4dee64ac8
Ryan O'Day <romanguy722@gmail.com>
2016-01-26 22:45:00 -0500

    dlgVolume UI and code outline

--- src/Aire.API/Dialogs/Volume/dlgVolume.Designer.vb
+++ src/Aire.API/Dialogs/Volume/dlgVolume.Designer.vb
@@ -22,20 +22,51 @@ Partial Class dlgVolume
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.lbl_vol = New System.Windows.Forms.Label()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(32, 12)
+        Me.TrackBar1.Maximum = 100
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.TrackBar1.Size = New System.Drawing.Size(45, 136)
+        Me.TrackBar1.TabIndex = 0
+        Me.TrackBar1.TickStyle = System.Windows.Forms.TickStyle.None
+        '
+        'lbl_vol
+        '
+        Me.lbl_vol.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.lbl_vol.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_vol.ForeColor = System.Drawing.SystemColors.Control
+        Me.lbl_vol.Location = New System.Drawing.Point(0, 151)
+        Me.lbl_vol.Name = "lbl_vol"
+        Me.lbl_vol.Size = New System.Drawing.Size(89, 23)
+        Me.lbl_vol.TabIndex = 1
+        Me.lbl_vol.Text = "0%"
+        Me.lbl_vol.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'dlgVolume
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.BackColor = System.Drawing.Color.DarkSlateGray
+        Me.ClientSize = New System.Drawing.Size(89, 174)
+        Me.Controls.Add(Me.lbl_vol)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "dlgVolume"
         Me.ShowIcon = False
         Me.ShowInTaskbar = False
-        Me.Text = "dlgVolume"
         Me.TopMost = True
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents TrackBar1 As System.Windows.Forms.TrackBar
+    Friend WithEvents lbl_vol As System.Windows.Forms.Label
 End Class
--- src/Aire.API/Dialogs/Volume/dlgVolume.vb
+++ src/Aire.API/Dialogs/Volume/dlgVolume.vb
@@ -1,3 +1,27 @@
 ﻿Friend Class dlgVolume
 
+#Region "Methods"
+
+
+
+#End Region
+
+#Region "UI"
+
+
+
+#End Region
+
+#Region "dlgVolume"
+
+    Private Sub dlgVolume_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub dlgVolume_LostFocus(sender As Object, e As EventArgs) Handles Me.LostFocus
+
+    End Sub
+
+#End Region
+
 End Class
