036ed6ab0c596f9df1bedd5d1f324dba6ed4459c
Ryan O'Day <romanguy722@gmail.com>
2016-01-26 22:39:20 -0500

    dlgVolumeStatus checks and displays if audio is muted

--- src/Aire.API/Dialogs/Volume/dlgVolumeStatus.vb
+++ src/Aire.API/Dialogs/Volume/dlgVolumeStatus.vb
@@ -13,7 +13,7 @@ Friend Class dlgVolumeStatus
         Me.Invalidate()
     End Sub
 
-    Private Sub DrawProgress(g As Graphics, rect As Rectangle, percentage As Single)
+    Private Sub DrawProgress(g As Graphics, rect As Rectangle, percentage As Single, Optional ByVal mute As Boolean = False)
         Dim progressAngle = CSng(360 / 100 * percentage)
         Dim remainderAngle = 360 - progressAngle
         Using progressPen As New Pen(Color.LightSeaGreen, 2), remainderPen As New Pen(Color.LightGray, 2)
@@ -22,10 +22,16 @@ Friend Class dlgVolumeStatus
             g.DrawArc(remainderPen, rect, progressAngle - 90, remainderAngle)
         End Using
         Using fnt As New Font(Me.Font.FontFamily, 14)
-            Dim text As String = percentage.ToString + "%"
-            Dim textSize = g.MeasureString(text, fnt)
+            Dim text As String
+            If mute Then
+                text = "Muted"
+            Else
+                text = percentage.ToString + "%"
+            End If
+
+            Dim textSize = g.MeasureString(Text, fnt)
             Dim textPoint As New Point(CInt(rect.Left + (rect.Width / 2) - (textSize.Width / 2)), CInt(rect.Top + (rect.Height / 2) - (textSize.Height / 2)))
-            g.DrawString(text, fnt, Brushes.Black, textPoint)
+            g.DrawString(Text, fnt, Brushes.Black, textPoint)
         End Using
     End Sub
 
@@ -45,7 +51,7 @@ Friend Class dlgVolumeStatus
     End Sub
 
     Private Sub dlgVolumeStatus_Paint(sender As Object, e As Windows.Forms.PaintEventArgs) Handles Me.Paint
-        DrawProgress(e.Graphics, New Rectangle(5, 5, 60, 60), Volume)
+        DrawProgress(e.Graphics, New Rectangle(5, 5, 60, 60), Volume, Audio.Volume.GetIfMuted)
     End Sub
 
 #End Region
