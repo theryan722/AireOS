2821095b17580388ad3038b5f34d27784e733462
Ryan O'Day <romanguy722@gmail.com>
2016-01-27 18:04:11 -0500

    Regions and basic outline for launchbar and appbar

--- src/Aire.Desktop/Forms/Bars/bar_appbar.vb
+++ src/Aire.Desktop/Forms/Bars/bar_appbar.vb
@@ -1,3 +1,29 @@
 ﻿Public Class bar_appbar
 
+#Region "Variables/Properties"
+
+    Private desktop As frmDesktop
+
+#End Region
+
+#Region "UI"
+
+
+
+#End Region
+
+#Region "Methods"
+
+
+
+#End Region
+
+#Region "bar_appbar"
+
+    Public Sub New(ByVal frm As frmDesktop)
+        desktop = frm
+    End Sub
+
+#End Region
+
 End Class
--- src/Aire.Desktop/Forms/Bars/bar_launchbar.vb
+++ src/Aire.Desktop/Forms/Bars/bar_launchbar.vb
@@ -1,3 +1,29 @@
 ﻿Public Class bar_launchbar
 
+#Region "Variables/Properties"
+
+    Private desktop As frmDesktop
+
+#End Region
+
+#Region "UI"
+
+
+
+#End Region
+
+#Region "Methods"
+
+
+
+#End Region
+
+#Region "bar_appbar"
+
+    Public Sub New(ByVal frm As frmDesktop)
+        desktop = frm
+    End Sub
+
+#End Region
+
 End Class
