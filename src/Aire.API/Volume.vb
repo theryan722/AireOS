5f79e178c847ea31650f0ba4a7ead658f1b3ca14
Ryan O'Day <romanguy722@gmail.com>
2016-01-26 21:27:21 -0500

    RaiseEvent volume changed

--- src/Aire.API/Volume.vb
+++ src/Aire.API/Volume.vb
@@ -4,22 +4,27 @@
 
         Public Shared Sub Increase(ByVal amount As Integer)
             Sys.Process.ExecuteCommand("pactl", "-- set-sink-volume 0 +" & Math.Min(amount, 100) & "%")
+            Sys.Events.Raise_VolumeChanged(GetVolume())
         End Sub
 
         Public Shared Sub Decrease(ByVal amount As Integer)
             Sys.Process.ExecuteCommand("pactl", "-- set-sink-volume 0 -" & Math.Min(amount, 100) & "%")
+            Sys.Events.Raise_VolumeChanged(GetVolume())
         End Sub
 
         Public Shared Sub Mute()
             Sys.Process.ExecuteCommand("pactl", "-- set-sink-mute 0 1")
+            Sys.Events.Raise_VolumeChanged(GetVolume())
         End Sub
 
         Public Shared Sub Unmute()
             Sys.Process.ExecuteCommand("pactl", "-- set-sink-mute 0 0")
+            Sys.Events.Raise_VolumeChanged(GetVolume())
         End Sub
 
         Public Shared Sub ToggleMute()
             Sys.Process.ExecuteCommand("pactl", "-- set-sink-mute 0 toggle")
+            Sys.Events.Raise_VolumeChanged(GetVolume())
         End Sub
 
         Public Shared Function GetVolume() As Integer
