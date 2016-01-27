76111612b87794a716c4a4a0fc8aa0cbda3017cc
Ryan O'Day <romanguy722@gmail.com>
2016-01-27 17:25:18 -0500

    Desktop manager class outline

--- src/Aire.Desktop/Classes/Manager.vb
+++ src/Aire.Desktop/Classes/Manager.vb
@@ -1,5 +1,15 @@
 ﻿Public Class Manager
 
+    Public Shared Sub AddDesktop()
 
+    End Sub
+
+    Public Shared Sub RemoveDesktop(ByVal desktop As frmDesktop)
+
+    End Sub
+
+    Public Shared Sub GoToDesktop(ByVal desktop As frmDesktop)
+
+    End Sub
 
 End Class
