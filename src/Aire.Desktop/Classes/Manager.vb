de3d296e53c390302a3e7920f8e005f21220b850
Ryan O'Day <romanguy722@gmail.com>
2016-01-28 12:17:13 -0500

    Outline of ShowMainDesktop()

--- src/Aire.Desktop/Classes/Manager.vb
+++ src/Aire.Desktop/Classes/Manager.vb
@@ -1,5 +1,11 @@
 ﻿Public Class Manager
 
+    Public Shared Sub ShowMainDesktop(ByVal user As String, ByVal pass As String)
+        If Aire.API.User.Users.Actions.MatchPasswordForUser(user, pass) Then
+
+        End If
+    End Sub
+
     Public Shared Sub AddDesktop()
 
     End Sub
