7a8ca645714fcaf5a82d0634814aa0142d989414
Ryan O'Day <romanguy722@gmail.com>
2016-01-27 19:05:57 -0500

    Handle frmLauncher load, closing events

--- src/Aire.Desktop/Forms/frmLauncher.vb
+++ src/Aire.Desktop/Forms/frmLauncher.vb
@@ -20,7 +20,7 @@
 
 #End Region
 
-#Region "bar_appbar"
+#Region "frmLauncher"
 
     Public Sub New(ByVal frm As bar_launchbar, ByVal frm2 As frmDesktop)
         InitializeComponent()
@@ -28,6 +28,15 @@
         desktop = frm2
     End Sub
 
+    Private Sub frmLauncher_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        frmLauncher.Showing = False
+    End Sub
+
+    Private Sub frmLauncher_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        frmLauncher.Showing = True
+    End Sub
+
 #End Region
 
+    
 End Class
