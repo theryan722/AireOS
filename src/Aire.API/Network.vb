ae69d5d5844d3e50bd0bd7e73c4ff0e52bc20556
Ryan O'Day <romanguy722@gmail.com>
2016-01-27 23:41:16 -0500

    Surround network api with try catch blocks

--- src/Aire.API/Network.vb
+++ src/Aire.API/Network.vb
@@ -5,31 +5,51 @@
         Public Class Info
 
             Public Shared Function GetIfWireless() As Boolean
-                Dim res As String = Sys.Process.ExecuteScriptWithOutput(Aire.DataManager.DataPaths.GetSysDataLocation & "/Scripts/getnetworktype.sh")
-                Return res = "WIRELESS"
+                Try
+                    Dim res As String = Sys.Process.ExecuteScriptWithOutput(Aire.DataManager.DataPaths.GetSysDataLocation & "/Scripts/getnetworktype.sh")
+                    Return res = "WIRELESS"
+                Catch ex As Exception
+                    Sys.Logging.Log.Write("ERROR: ", ex.ToString)
+                    Return "ERROR"
+                End Try
             End Function
 
             Public Shared Function GetIfNetworkConnection() As Boolean
-                Return Sys.Info.Network.IsAvailable()
+                Try
+                    Return Sys.Info.Network.IsAvailable()
+                Catch ex As Exception
+                    Sys.Logging.Log.Write("ERROR: ", ex.ToString)
+                    Return Nothing
+                End Try
             End Function
 
             Public Shared Function GetWirelessStrengthDBM() As Integer
-                Dim res As String = Sys.Process.ExecuteCommandWithOutput("iwconfig", "eth1")
-                Return CInt(res.Substring(res.IndexOf("level=") + 6, 4).Trim(" "))
+                Try
+                    Dim res As String = Sys.Process.ExecuteCommandWithOutput("iwconfig", "eth1")
+                    Return CInt(res.Substring(res.IndexOf("level=") + 6, 4).Trim(" "))
+                Catch ex As Exception
+                    Sys.Logging.Log.Write("ERROR: ", ex.ToString)
+                    Return Nothing
+                End Try
             End Function
 
             Public Shared Function GetWirelessStrengthInBars() As Integer
-                Dim res As String = Sys.Process.ExecuteCommandWithOutput("iwconfig", "eth1")
-                Dim dbm As Integer = CInt(res.Substring(res.IndexOf("level=") + 6, 4).Trim(" "))
-                If dbm > -50 Then
-                    Return 4
-                ElseIf dbm > -60 Then
-                    Return 3
-                ElseIf dbm > -70 Then
-                    Return 2
-                Else
-                    Return 1
-                End If
+                Try
+                    Dim res As String = Sys.Process.ExecuteCommandWithOutput("iwconfig", "eth1")
+                    Dim dbm As Integer = CInt(res.Substring(res.IndexOf("level=") + 6, 4).Trim(" "))
+                    If dbm > -50 Then
+                        Return 4
+                    ElseIf dbm > -60 Then
+                        Return 3
+                    ElseIf dbm > -70 Then
+                        Return 2
+                    Else
+                        Return 1
+                    End If
+                Catch ex As Exception
+                    Sys.Logging.Log.Write("ERROR: ", ex.ToString)
+                    Return Nothing
+                End Try
             End Function
 
         End Class
