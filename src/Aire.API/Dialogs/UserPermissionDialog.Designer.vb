242ad214fbaf551fa35be96d720315e503e9b196
Ryan O'Day <romanguy722@gmail.com>
2016-01-28 12:24:22 -0500

    UserPermissionDialog() UI and code regions

---
+++ src/Aire.API/Dialogs/UserPermissionDialog.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UserPermissionDialog
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnOk = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.txt_username = New System.Windows.Forms.TextBox()
+        Me.txt_password = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lbl_reqperm = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lb_users = New System.Windows.Forms.ListBox()
+        Me.btnUsers = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnOk
+        '
+        Me.btnOk.Dock = System.Windows.Forms.DockStyle.Left
+        Me.btnOk.Location = New System.Drawing.Point(0, 0)
+        Me.btnOk.Name = "btnOk"
+        Me.btnOk.Size = New System.Drawing.Size(75, 23)
+        Me.btnOk.TabIndex = 0
+        Me.btnOk.Text = "Ok"
+        Me.btnOk.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Dock = System.Windows.Forms.DockStyle.Right
+        Me.btnCancel.Location = New System.Drawing.Point(273, 0)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancel.TabIndex = 1
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'txt_username
+        '
+        Me.txt_username.Location = New System.Drawing.Point(76, 51)
+        Me.txt_username.Name = "txt_username"
+        Me.txt_username.Size = New System.Drawing.Size(138, 20)
+        Me.txt_username.TabIndex = 2
+        '
+        'txt_password
+        '
+        Me.txt_password.Location = New System.Drawing.Point(76, 77)
+        Me.txt_password.Name = "txt_password"
+        Me.txt_password.PasswordChar = Global.Microsoft.VisualBasic.ChrW(8226)
+        Me.txt_password.Size = New System.Drawing.Size(138, 20)
+        Me.txt_password.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 51)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Username:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(14, 77)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Password:"
+        '
+        'lbl_reqperm
+        '
+        Me.lbl_reqperm.AutoSize = True
+        Me.lbl_reqperm.Location = New System.Drawing.Point(14, 108)
+        Me.lbl_reqperm.Name = "lbl_reqperm"
+        Me.lbl_reqperm.Size = New System.Drawing.Size(135, 13)
+        Me.lbl_reqperm.TabIndex = 6
+        Me.lbl_reqperm.Text = "Required Permission Level:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(257, 26)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Please enter the details of a user with the appropriate" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "permission level to conf" & _
+    "irm this action."
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.btnCancel)
+        Me.Panel1.Controls.Add(Me.btnOk)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel1.Location = New System.Drawing.Point(0, 131)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(348, 23)
+        Me.Panel1.TabIndex = 8
+        '
+        'lb_users
+        '
+        Me.lb_users.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lb_users.FormattingEnabled = True
+        Me.lb_users.ItemHeight = 20
+        Me.lb_users.Location = New System.Drawing.Point(228, 46)
+        Me.lb_users.Name = "lb_users"
+        Me.lb_users.ScrollAlwaysVisible = True
+        Me.lb_users.Size = New System.Drawing.Size(120, 84)
+        Me.lb_users.TabIndex = 9
+        Me.lb_users.Visible = False
+        '
+        'btnUsers
+        '
+        Me.btnUsers.Location = New System.Drawing.Point(305, 23)
+        Me.btnUsers.Name = "btnUsers"
+        Me.btnUsers.Size = New System.Drawing.Size(43, 23)
+        Me.btnUsers.TabIndex = 10
+        Me.btnUsers.Text = "Users"
+        Me.btnUsers.UseVisualStyleBackColor = True
+        Me.btnUsers.Visible = False
+        '
+        'UserPermissionDialog
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(348, 154)
+        Me.Controls.Add(Me.btnUsers)
+        Me.Controls.Add(Me.lb_users)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lbl_reqperm)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt_password)
+        Me.Controls.Add(Me.txt_username)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "UserPermissionDialog"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "User Permission"
+        Me.TopMost = True
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnOk As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+    Friend WithEvents txt_username As System.Windows.Forms.TextBox
+    Friend WithEvents txt_password As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lbl_reqperm As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents lb_users As System.Windows.Forms.ListBox
+    Friend WithEvents btnUsers As System.Windows.Forms.Button
+End Class
---
+++ src/Aire.API/Dialogs/UserPermissionDialog.vb
@@ -0,0 +1,27 @@
+﻿Public Class UserPermissionDialog
+
+#Region "Properties/Variables"
+
+
+
+#End Region
+
+#Region "UI"
+
+
+
+#End Region
+
+#Region "Methods"
+
+
+
+#End Region
+
+#Region "UserPermissionDialog"
+
+
+
+#End Region
+
+End Class
