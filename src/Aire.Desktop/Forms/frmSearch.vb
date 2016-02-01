de99695dc36609e080391e8a42f6ffc6d8e87d4a
Ryan O'Day <romanguy722@gmail.com>
2016-02-01 15:55:06 -0500

    frmSearch regions

--- src/Aire.Desktop/Forms/frmSearch.vb
+++ src/Aire.Desktop/Forms/frmSearch.vb
@@ -1,3 +1,29 @@
 ﻿Public Class frmSearch
 
+#Region "Properties/Variables"
+
+
+
+#End Region
+
+#Region "UI"
+
+
+
+#End Region
+
+#Region "Methods"
+
+
+
+#End Region
+
+#Region "frmSearch"
+
+    Private Sub frmSearch_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+#End Region
+    
 End Class
