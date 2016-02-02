9ee6ee379c197ccf76c55121c0a6c34bc1dc9859
Ryan O'Day <romanguy722@gmail.com>
2016-02-01 22:17:06 -0500

    Outline more of LaunchBarItem, basic UI

--- src/Aire.Desktop/UserControls/LaunchBarItem.Designer.vb
+++ src/Aire.Desktop/UserControls/LaunchBarItem.Designer.vb
@@ -22,16 +22,40 @@ Partial Class LaunchBarItem
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
         Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lblName.Location = New System.Drawing.Point(0, 0)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(13, 13)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "[]"
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
         '
         'LaunchBarItem
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.lblName)
         Me.Name = "LaunchBarItem"
-        Me.Size = New System.Drawing.Size(101, 37)
+        Me.Size = New System.Drawing.Size(80, 30)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents ToolTip1 As System.Windows.Forms.ToolTip
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
 
 End Class
--- src/Aire.Desktop/UserControls/LaunchBarItem.vb
+++ src/Aire.Desktop/UserControls/LaunchBarItem.vb
@@ -25,14 +25,30 @@
 
 #Region "UI"
 
+    Private Sub lblName_Click(sender As Object, e As EventArgs) Handles lblName.Click
 
+    End Sub
+
+    Private Sub lblName_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles lblName.MouseDoubleClick
+
+    End Sub
 
 #End Region
 
 #Region "LaunchBarItem"
 
+    Private Sub LaunchBarItem_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub LaunchBarItem_MouseClick(sender As Object, e As MouseEventArgs) Handles Me.MouseClick
+
+    End Sub
+
+    Private Sub LaunchBarItem_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles Me.MouseDoubleClick
+
+    End Sub
 
 #End Region
-
+    
 End Class
