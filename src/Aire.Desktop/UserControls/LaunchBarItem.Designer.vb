90861f684c4329117e9ddaf3209a784e1ae731bc
Ryan O'Day <romanguy722@gmail.com>
2016-02-01 21:43:41 -0500

    LaunchBarItem regions, some properties

--- src/Aire.Desktop/UserControls/LaunchBarItem.Designer.vb
+++ src/Aire.Desktop/UserControls/LaunchBarItem.Designer.vb
@@ -22,8 +22,16 @@ Partial Class LaunchBarItem
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'LaunchBarItem
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Name = "LaunchBarItem"
+        Me.Size = New System.Drawing.Size(101, 37)
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
--- src/Aire.Desktop/UserControls/LaunchBarItem.vb
+++ src/Aire.Desktop/UserControls/LaunchBarItem.vb
@@ -1,3 +1,38 @@
 ﻿Public Class LaunchBarItem
 
+#Region "Properties"
+
+    Public Shadows Property BackColor As Color
+        Get
+
+        End Get
+        Set(value As Color)
+
+        End Set
+    End Property
+
+    Public Property Window As String
+
+    Public Shadows Property Text As String
+
+#End Region
+
+#Region "Methods"
+
+
+
+#End Region
+
+#Region "UI"
+
+
+
+#End Region
+
+#Region "LaunchBarItem"
+
+
+
+#End Region
+
 End Class
