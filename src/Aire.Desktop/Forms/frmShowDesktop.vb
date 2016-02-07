66ed61ec8d6a08aa8e98c6d094b913d5425ef33b
Ryan O'Day <romanguy722@gmail.com>
2016-02-07 18:04:45 -0500

    Event outline

--- src/Aire.Desktop/Forms/frmShowDesktop.vb
+++ src/Aire.Desktop/Forms/frmShowDesktop.vb
@@ -16,7 +16,21 @@
 
 #Region "frmShowDesktop"
 
+    Private Sub frmShowDesktop_MouseClick(sender As Object, e As MouseEventArgs) Handles Me.MouseClick
 
+    End Sub
+
+    Private Sub frmShowDesktop_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseDown
+
+    End Sub
+
+    Private Sub frmShowDesktop_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseMove
+
+    End Sub
+
+    Private Sub frmShowDesktop_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseUp
+
+    End Sub
 
 #End Region
 
