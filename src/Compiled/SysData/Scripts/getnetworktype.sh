ddf18bce6bbc133f58e02f51a08ee9223aa0155d
Ryan O'Day <romanguy722@gmail.com>
2016-02-07 18:47:36 -0500

    Fix script

--- src/Compiled/SysData/Scripts/getnetworktype.sh
+++ src/Compiled/SysData/Scripts/getnetworktype.sh
@@ -1,19 +1,2 @@
 #!/bin/bash
-
-if ! /bin/ip route | grep -q ^default; then
-  echo "DISCONNECTED"
-  exit 0
-fi
-if="$(/bin/ip route | 
-  awk '$1 == "default" {for (i=2;i<=NF;i++) if ($i == "dev") { i++;print $i; exit}}')"
-if [ -z "$if" -o \! -e /sys/class/net/"$if" ]; then
-  echo "ERROR"
-  exit 1
-fi
-if /usr/sbin/iw dev "$if" info &>/dev/null; then
-  echo "WIRELESS"
-  # uncomment the next line to start iwconfig
-  # iwconfig
-else
-  echo "WIRED"
-fi
+ip route get 8.8.8.8 | grep -Po 'dev \K\w+' | grep -qFf - /proc/net/wireless && echo wireless || echo wired
