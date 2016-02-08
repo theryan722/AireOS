ec05133dc13182eb9a077769ef52ee67ad969bb4
Ryan O'Day <romanguy722@gmail.com>
2016-02-07 20:44:55 -0500

    Fix Aire.Host graphical artifact

--- src/Aire.Host/frmMain.Designer.vb
+++ src/Aire.Host/frmMain.Designer.vb
@@ -29,7 +29,7 @@ Partial Class frmMain
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.ClientSize = New System.Drawing.Size(10, 10)
+        Me.ClientSize = New System.Drawing.Size(161, 63)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.MaximizeBox = False
         Me.MinimizeBox = False
