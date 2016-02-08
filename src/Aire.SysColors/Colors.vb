d375fb91e65f3fe7e32022af0e35ea50b8827143
Ryan O'Day <romanguy722@gmail.com>
2016-02-07 21:13:11 -0500

    ActiveCaptionText

--- src/Aire.SysColors/Colors.vb
+++ src/Aire.SysColors/Colors.vb
@@ -4,5 +4,6 @@ Public Class Colors
 
     Public Shared ActiveBorder As Color = Color.FromArgb(180, 180, 180)
     Public Shared ActiveCaption As Color = Color.FromArgb(153, 180, 209)
+    Public Shared ActiveCaptionText As Color = Color.FromArgb(0, 0, 0)
 
 End Class
