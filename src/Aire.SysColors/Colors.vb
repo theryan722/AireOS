f24504ccf8a9d4da73252d6495525fbaa5241b0d
Ryan O'Day <romanguy722@gmail.com>
2016-02-07 21:13:46 -0500

    ControlDark

--- src/Aire.SysColors/Colors.vb
+++ src/Aire.SysColors/Colors.vb
@@ -7,5 +7,6 @@ Public Class Colors
     Public Shared ActiveCaptionText As Color = Color.FromArgb(0, 0, 0)
     Public Shared AppWorkspace As Color = Color.FromArgb(171, 171, 171)
     Public Shared Control As Color = Color.FromArgb(240, 240, 240)
+    Public Shared ControlDark As Color = Color.FromArgb(160, 160, 160)
 
 End Class
