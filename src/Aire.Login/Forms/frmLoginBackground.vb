f34ea126eda952d18a5c715b7d6c766521bd27bb
Ryan O'Day <romanguy722@gmail.com>
2016-02-07 21:23:07 -0500

    Fix mono color discrepancy in frmLoginBackground

--- src/Aire.Login/Forms/frmLoginBackground.vb
+++ src/Aire.Login/Forms/frmLoginBackground.vb
@@ -1,6 +1,7 @@
 ﻿Public Class frmLoginBackground
 
     Private Sub frmLoginBackground_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.BackColor = Aire.SysColors.Colors.ActiveCaption
         frmLogin.Show()
         frmLogin.LBForm = Me
     End Sub
