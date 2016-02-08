d12eb456ac96f64c183edad5957f3d6886c6206d
Ryan O'Day <romanguy722@gmail.com>
2016-02-07 21:13:21 -0500

    AppWorkspace

--- src/Aire.SysColors/Colors.vb
+++ src/Aire.SysColors/Colors.vb
@@ -5,5 +5,6 @@ Public Class Colors
     Public Shared ActiveBorder As Color = Color.FromArgb(180, 180, 180)
     Public Shared ActiveCaption As Color = Color.FromArgb(153, 180, 209)
     Public Shared ActiveCaptionText As Color = Color.FromArgb(0, 0, 0)
+    Public Shared AppWorkspace As Color = Color.FromArgb(171, 171, 171)
 
 End Class
