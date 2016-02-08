276b1e331710253b006d57566aeeb633aeb800fb
Ryan O'Day <romanguy722@gmail.com>
2016-02-07 21:15:43 -0500

    InactiveBorder

--- src/Aire.SysColors/Colors.vb
+++ src/Aire.SysColors/Colors.vb
@@ -17,5 +17,6 @@ Public Class Colors
     Public Shared Highlight As Color = Color.FromArgb(51, 153, 255)
     Public Shared HighlightText As Color = Color.FromArgb(255, 255, 255)
     Public Shared HotTrack As Color = Color.FromArgb(0, 102, 204)
+    Public Shared InactiveBorder As Color = Color.FromArgb(244, 247, 252)
 
 End Class
