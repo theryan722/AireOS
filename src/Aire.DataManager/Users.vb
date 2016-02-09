36074aeeaf8737b29d8664b6552513c857bfd027
Ryan O'Day <romanguy722@gmail.com>
2016-02-09 15:45:53 -0500

    Split SetupUserDataStorage into smaller helper methods

--- src/Aire.DataManager/Users.vb
+++ src/Aire.DataManager/Users.vb
@@ -1,14 +1,30 @@
 ﻿Public Class Users
 
-    Public Shared Sub SetupUserDataStorage(ByVal loc As String)
+#Region "Helper"
+
+    Private Shared Sub CreateDirectories(ByVal loc As String)
         System.IO.Directory.CreateDirectory(loc)
         System.IO.Directory.CreateDirectory(loc & "/Apps")
         System.IO.Directory.CreateDirectory(loc & "/Apps/Terminal")
+    End Sub
+
+    Private Shared Sub CreateFiles(ByVal loc As String)
         System.IO.File.Create(loc & "/log.txt")
         System.IO.File.Create(loc & "/Apps/Terminal/config.txt")
         System.IO.File.Create(loc & "/Apps/Terminal/external.txt")
+    End Sub
+
+    Private Shared Sub CopyDefaultFiles(ByVal loc As String)
         System.IO.File.Copy(DataPaths.GetSysDataLocation & "/Default/defbg.jpg", loc & "/bg.jpg")
         System.IO.File.Copy(DataPaths.GetSysDataLocation & "/Default/defprofile.jpg", loc & "/profile.jpg")
+    End Sub
+
+#End Region
+
+    Public Shared Sub SetupUserDataStorage(ByVal loc As String)
+        CreateDirectories(loc)
+        CreateFiles(loc)
+        CopyDefaultFiles(loc)
     End Sub
 
 End Class
