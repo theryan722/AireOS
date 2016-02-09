b423e0e5bb21fe5f6857c6322a95334d3f58a0d9
Ryan O'Day <romanguy722@gmail.com>
2016-02-09 18:16:13 -0500

    Add timestamp to notifications

--- src/Aire.API/Notifications.vb
+++ src/Aire.API/Notifications.vb
@@ -2,7 +2,7 @@
 
     Public Shared Sub Add(ByVal txt As String, Optional ByVal displaytoast As Boolean = False)
         Dim objWriter As New System.IO.StreamWriter(Aire.DataManager.DataPaths.GetSysDataLocation & "/notifications.txt", True)
-        objWriter.Write(vbNewLine & txt)
+        objWriter.Write(vbNewLine & DateTime.Now.ToString & "|" & txt)
         objWriter.Close()
         If displaytoast Then
             Dim bb As New Toast(txt, 2500, "Notification")
