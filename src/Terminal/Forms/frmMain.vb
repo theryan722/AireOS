1be0a78767be46f710be73e36076225109432721
Ryan O'Day <romanguy722@gmail.com>
2016-02-09 15:29:16 -0500

    Fix not calling class for CheckAndRepairData()

--- src/Terminal/Forms/frmMain.vb
+++ src/Terminal/Forms/frmMain.vb
@@ -155,7 +155,7 @@
 #Region "frmMain"
 
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        CheckAndRepairData()
+        DataManager.CheckAndRepairData()
         TextBox1.Text = "Terminal@" & Aire.API.User.Current.Info.GetUsername & ":>"
         LoadSettings()
     End Sub
