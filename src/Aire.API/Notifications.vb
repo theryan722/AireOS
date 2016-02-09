e8fbeff5244d317c83658e2012c65758f5c2a6f2
Ryan O'Day <romanguy722@gmail.com>
2016-02-09 18:15:05 -0500

    Update Aire.API.Notifications.Add()

--- src/Aire.API/Notifications.vb
+++ src/Aire.API/Notifications.vb
@@ -1,9 +1,12 @@
 ﻿Public Class Notifications
 
-    Public Shared Sub Add(ByVal txt As String)
+    Public Shared Sub Add(ByVal txt As String, Optional ByVal displaytoast As Boolean = False)
         Dim objWriter As New System.IO.StreamWriter(Aire.DataManager.DataPaths.GetSysDataLocation & "/notifications.txt", True)
         objWriter.Write(vbNewLine & txt)
         objWriter.Close()
+        If displaytoast Then
+            Dim bb As New Toast(txt, 2500, "Notification")
+        End If
     End Sub
 
 End Class
