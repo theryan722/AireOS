eab281eeae7e84de7cc53fc59705b450b26e7c7d
Ryan O'Day <romanguy722@gmail.com>
2016-02-09 14:44:56 -0500

    Regions, add variables

--- src/Terminal/Forms/dlgOptions.vb
+++ src/Terminal/Forms/dlgOptions.vb
@@ -1,3 +1,28 @@
 ﻿Public Class dlgOptions
 
+#Region "Variables"
+
+    Private fcolor As Color
+    Private bcolor As Color
+
+#End Region
+
+#Region "UI"
+
+
+
+#End Region
+
+#Region "Methods"
+
+
+
+#End Region
+
+#Region "dlgOptions"
+
+
+
+#End Region
+
 End Class
