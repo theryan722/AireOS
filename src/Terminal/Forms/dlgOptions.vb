79a4a0546fa5c5f1731e43b7bd3c52c4ca285d18
Ryan O'Day <romanguy722@gmail.com>
2016-02-09 14:53:35 -0500

    Implement btnCancel

--- src/Terminal/Forms/dlgOptions.vb
+++ src/Terminal/Forms/dlgOptions.vb
@@ -11,7 +11,7 @@
     End Sub
 
     Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
-
+        Me.Close()
     End Sub
 
     Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
