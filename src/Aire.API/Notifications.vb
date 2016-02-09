13033bc6e50e369ff311e41076ec7b15f49e3525
Ryan O'Day <romanguy722@gmail.com>
2016-02-09 18:13:47 -0500

    Remove Aire.NotificationManager, Aire.Notifications API

---
+++ src/Aire.API/Notifications.vb
@@ -0,0 +1,9 @@
+﻿Public Class Notifications
+
+    Public Shared Sub Add(ByVal txt As String)
+        Dim objWriter As New System.IO.StreamWriter(Aire.DataManager.DataPaths.GetSysDataLocation & "/notifications.txt", True)
+        objWriter.Write(vbNewLine & txt)
+        objWriter.Close()
+    End Sub
+
+End Class
