ed448640b79c5566df4d8a658ef9c586eaf635c8
Ryan O'Day <romanguy722@gmail.com>
2016-02-09 17:33:24 -0500

    Prepare for first time setup

--- src/AireInit/Launch.vb
+++ src/AireInit/Launch.vb
@@ -1,12 +1,16 @@
 ﻿Module Launch
     Private hostloc As String = Aire.API.Compatibility.FixPath(AppDomain.CurrentDomain.BaseDirectory & "/Aire.Host.exe")
-
+    Private setuploc As String = Aire.API.Compatibility.FixPath(AppDomain.CurrentDomain.BaseDirectory & "/Aire.FirstTimeSetup.exe")
     'Initializes Aire OS
     Sub Main()
         Console.Title = "Aire OS Initialization"
         Aire.API.Sys.Logging.Log.Write("System is initializing.")
         InitializeAudio()
+        'If System.IO.File.Exists(setuploc) Then
+        '    Process.Start(setuploc)
+        'Else
         Process.Start(hostloc)
+        'End If
     End Sub
 
     Private Sub InitializeAudio()
