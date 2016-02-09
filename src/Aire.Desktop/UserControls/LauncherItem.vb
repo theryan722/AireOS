2e2165a455e38289af649411f4ea66ce183eb45f
Ryan O'Day <romanguy722@gmail.com>
2016-02-09 17:43:43 -0500

    Implement Title Property

--- src/Aire.Desktop/UserControls/LauncherItem.vb
+++ src/Aire.Desktop/UserControls/LauncherItem.vb
@@ -5,10 +5,17 @@
     Private ftitle As String
     Public Property Title As String
         Get
-
+            Return ftitle
         End Get
         Set(value As String)
-
+            ftitle = value
+            If value.Length > 20 Then
+                lblText.Text = value.Substring(0, 20) & "..."
+            Else
+                lblText.Text = value
+            End If
+            ToolTip1.SetToolTip(lblText, value)
+            ToolTip1.SetToolTip(Me, value)
         End Set
     End Property
     Public Property FileLocation As String
