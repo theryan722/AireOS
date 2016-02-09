423576a6892bca8df7f14ab3027210e3bc8b6c3d
Ryan O'Day <romanguy722@gmail.com>
2016-02-08 19:00:58 -0500

    Outline several commands

--- src/Terminal/Classes/CommandManager.vb
+++ src/Terminal/Classes/CommandManager.vb
@@ -3,10 +3,38 @@
     Public Shared Sub ExecuteCommand(ByVal cmd As String, ByVal cmdform As frmMain)
         Dim sText As String = ""
         Try
-            Select Case cmd
-                Case cmd.ToLower = "clear"
+            Select Case cmd.ToLower
+                Case "clear"
                     sText = ""
                     cmdform.TextBox1.Clear()
+                Case "exit"
+                Case "help"
+                Case "about"
+                Case "terminal -menu show"
+                Case "terminal -menu hide"
+                Case "terminal -topmost true"
+                Case "terminal -topmost false"
+                Case "terminal -windowstate minimize"
+                Case "terminal -windowstate maximize"
+                Case "terminal -windowstate normal"
+                Case cmd.ToLower.StartsWith("terminal -title")
+                Case cmd.ToLower.StartsWith("start")
+                Case cmd.ToLower.StartsWith("launch")
+                Case cmd.ToLower.StartsWith("get")
+                Case "aire -user logout"
+                Case "aire -user lock"
+                Case "aire -user switch"
+                Case "aire -power shutdown"
+                Case "aire -power restart"
+                Case "aire -power sleep"
+                Case "aire -power hibernate"
+                Case "aire -version"
+                Case cmd.ToLower.StartsWith("log -write")
+                Case cmd.ToLower.StartsWith("aire -notification toast")
+                Case cmd.ToLower.StartsWith("aire -notification msgbox")
+                Case cmd.ToLower.StartsWith("aire -volume -set")
+                Case "aire -volume -get"
+
                 Case Else
                     sText = "Unrecognized command. Enter HELP for help."
             End Select
