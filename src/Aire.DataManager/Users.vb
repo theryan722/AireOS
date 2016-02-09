fc799c2158431a0ba1996692baaa47e3c07422c9
Ryan O'Day <romanguy722@gmail.com>
2016-02-09 14:17:17 -0500

    Change directory name, outline ConfigManager

--- src/Aire.DataManager/Users.vb
+++ src/Aire.DataManager/Users.vb
@@ -2,7 +2,10 @@
 
     Public Shared Sub SetupUserDataStorage(ByVal loc As String)
         System.IO.Directory.CreateDirectory(loc)
+        System.IO.Directory.CreateDirectory(loc & "/Apps")
+        System.IO.Directory.CreateDirectory(loc & "/Apps/Terminal")
         System.IO.File.Create(loc & "/log.txt")
+        System.IO.File.Create(loc & "/Apps/Terminal/config.txt")
         System.IO.File.Copy(DataPaths.GetSysDataLocation & "/Default/defbg.jpg", loc & "/bg.jpg")
     End Sub
 
---
+++ src/Terminal/Classes/ConfigManager.vb
@@ -0,0 +1,53 @@
+﻿Public Class SettingsReader
+
+    Private Shared setloc As String = Aire.API.User.Current.Info.GetDataStorageLocation & "/Apps/Terminal/config.txt"
+
+#Region "Get"
+
+    Public Shared Function GetBackColor() As Color
+
+    End Function
+
+    Public Shared Function GetForeColor() As Color
+
+    End Function
+
+    Public Shared Function GetIfTopmost() As Boolean
+
+    End Function
+
+    Public Shared Function GetIfFullscreen() As Boolean
+
+    End Function
+
+    Public Shared Function GetIfAllowExternal() As Boolean
+
+    End Function
+
+#End Region
+
+#Region "Set"
+
+    Public Shared Sub SetBackColor(ByVal clr As Color)
+
+    End Sub
+
+    Public Shared Sub SetForeColor(ByVal clr As Color)
+
+    End Sub
+
+    Public Shared Sub SetIfTopmost(ByVal topmost As Boolean)
+
+    End Sub
+
+    Public Shared Sub SetIfFullscreen(ByVal fullscreen As Boolean)
+
+    End Sub
+
+    Public Shared Sub SetIfAllowExternal(ByVal allow As Boolean)
+
+    End Sub
+
+#End Region
+
+End Class
