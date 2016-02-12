72f7429c9ef2cdb15aa330c7a1ee9a710a8df9fd
Ryan O'Day <romanguy722@gmail.com>
2016-02-12 15:26:25 -0500

    Copy menu item calls related method

--- src/Notepad/frmMain.vb
+++ src/Notepad/frmMain.vb
@@ -108,7 +108,7 @@
     End Sub
 
     Private Sub CopyToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopyToolStripMenuItem.Click
-
+        Copy()
     End Sub
 
     Private Sub PasteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PasteToolStripMenuItem.Click
