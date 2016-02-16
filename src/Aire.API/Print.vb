93e3b3eb9aa0a4703a971acaee86fa8581622166
Ryan O'Day <romanguy722@gmail.com>
2016-02-16 17:33:53 -0500

    Print API outline

--- src/Aire.API/Print.vb
+++ src/Aire.API/Print.vb
@@ -2,13 +2,25 @@
 
     Public Class Info
 
+        Public Shared Function GetDefaultPrinter() As String
 
+        End Function
+
+        Public Shared Function GetPrinters() As List(Of String)
+
+        End Function
 
     End Class
 
     Public Class Actions
 
+        Public Shared Sub PrintFile(ByVal file As String, Optional ByVal printer As String = "", Optional ByVal printsettings As PrinterSettings = Nothing)
+           
+        End Sub
 
+        Public Shared Sub ClearPrintQeue()
+
+        End Sub
 
     End Class
 
