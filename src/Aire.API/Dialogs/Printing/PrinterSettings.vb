121dbf2b981cc5ca7ae37194921e9e1a95c841f0
Ryan O'Day <romanguy722@gmail.com>
2016-02-16 15:24:55 -0500

    PrinterSettings class, add regions, Media enum

---
+++ src/Aire.API/Dialogs/Printing/PrinterSettings.vb
@@ -0,0 +1,26 @@
+﻿Public Class PrinterSettings
+
+#Region "Enums"
+
+    Enum Media
+        Letter
+        Legal
+        A4
+        COM10
+        DL
+        Transparency
+        Upper
+        Lower
+        MultiPurpose
+        LargeCapacity
+    End Enum
+
+#End Region
+
+#Region "Properties"
+
+
+
+#End Region
+
+End Class
