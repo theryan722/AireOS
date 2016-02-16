8ab995426d876831d830ba3b4ec39fdda06b7af0
Ryan O'Day <romanguy722@gmail.com>
2016-02-16 18:07:02 -0500

    Begin PrintDialog UI

--- src/Aire.API/Dialogs/Printing/PrintDialog.Designer.vb
+++ src/Aire.API/Dialogs/Printing/PrintDialog.Designer.vb
@@ -22,8 +22,62 @@ Partial Class PrintDialog
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.pnl_bottom = New System.Windows.Forms.Panel()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.pnl_bottom.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnl_bottom
+        '
+        Me.pnl_bottom.Controls.Add(Me.btnPrint)
+        Me.pnl_bottom.Controls.Add(Me.btnCancel)
+        Me.pnl_bottom.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.pnl_bottom.Location = New System.Drawing.Point(0, 311)
+        Me.pnl_bottom.Name = "pnl_bottom"
+        Me.pnl_bottom.Size = New System.Drawing.Size(449, 23)
+        Me.pnl_bottom.TabIndex = 0
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Dock = System.Windows.Forms.DockStyle.Left
+        Me.btnCancel.Location = New System.Drawing.Point(0, 0)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancel.TabIndex = 1
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Dock = System.Windows.Forms.DockStyle.Right
+        Me.btnPrint.Location = New System.Drawing.Point(374, 0)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(75, 23)
+        Me.btnPrint.TabIndex = 2
+        Me.btnPrint.Text = "Print"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'PrintDialog
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "PrintDialog"
+        Me.ClientSize = New System.Drawing.Size(449, 334)
+        Me.Controls.Add(Me.pnl_bottom)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "PrintDialog"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Print"
+        Me.TopMost = True
+        Me.pnl_bottom.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents pnl_bottom As System.Windows.Forms.Panel
+    Friend WithEvents btnPrint As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
 End Class
