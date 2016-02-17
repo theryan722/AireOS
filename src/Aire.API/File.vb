26a7605aba9e4da4cb8ce2f8a3ff20dc45c838c1
Ryan O'Day <romanguy722@gmail.com>
2016-02-17 17:03:49 -0500

    Sys.IO Namespace, WriteText()

---
+++ src/Aire.API/File.vb
@@ -0,0 +1,15 @@
+﻿Imports System.IO
+
+Namespace Sys.IO
+
+    Public Class File
+
+        Public Shared Sub WriteText(ByVal file As String, ByVal text As String, Optional ByVal append As Boolean = False)
+            Using writer As StreamWriter = New StreamWriter(file, append)
+                writer.Write(text)
+            End Using
+        End Sub
+
+    End Class
+
+End Namespace
