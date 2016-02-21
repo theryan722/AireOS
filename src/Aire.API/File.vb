1f7e6b0409dea984eb326b6833a422ed6ed751c6
Ryan O'Day <romanguy722@gmail.com>
2016-02-21 15:35:36 -0500

    Comment File

--- src/Aire.API/File.vb
+++ src/Aire.API/File.vb
@@ -4,6 +4,13 @@ Namespace Sys.IO
 
     Public Class File
 
+        ''' <summary>
+        ''' Writes text to a file
+        ''' </summary>
+        ''' <param name="file">The file to write to</param>
+        ''' <param name="text">The text to write</param>
+        ''' <param name="append">Whether or not to append text to the file</param>
+        ''' <remarks></remarks>
         Public Shared Sub WriteText(ByVal file As String, ByVal text As String, Optional ByVal append As Boolean = False)
             Using writer As StreamWriter = New StreamWriter(file, append)
                 writer.Write(text)
