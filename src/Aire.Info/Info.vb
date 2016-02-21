064e144114abaf5f43e735b72b425c4d0ff496a7
Ryan O'Day <romanguy722@gmail.com>
2016-02-21 16:27:05 -0500

    Comment Info

--- src/Aire.Info/Info.vb
+++ src/Aire.Info/Info.vb
@@ -1,17 +1,37 @@
 ﻿Public Class Info
 
+    ''' <summary>
+    ''' The name of the operating system
+    ''' </summary>
+    ''' <returns>String, the name of the OS</returns>
+    ''' <remarks></remarks>
     Public Shared Function OSName() As String
         Return "Aire OS"
     End Function
 
+    ''' <summary>
+    ''' The version of the operating system
+    ''' </summary>
+    ''' <returns>String, the version of the OS</returns>
+    ''' <remarks></remarks>
     Public Shared Function OSVersion() As String
         Return "0.0.0.1"
     End Function
 
+    ''' <summary>
+    ''' The release info of the operating system
+    ''' </summary>
+    ''' <returns>String, the release info of the OS</returns>
+    ''' <remarks></remarks>
     Public Shared Function OSRelease() As String
         Return "Official|Pre-Alpha"
     End Function
 
+    ''' <summary>
+    ''' The website of the operating system
+    ''' </summary>
+    ''' <returns>String, the website of the OS</returns>
+    ''' <remarks></remarks>
     Public Shared Function Website() As String
         Return "https://github.com/theryan722/aireos"
     End Function
