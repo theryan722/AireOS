7d0390dfd335606acf3322b9bdb2d98d00d809a6
Ryan O'Day <romanguy722@gmail.com>
2016-02-21 15:00:15 -0500

    Comment Printer

--- src/Aire.API/Dialogs/Printing/Printer.vb
+++ src/Aire.API/Dialogs/Printing/Printer.vb
@@ -1,8 +1,27 @@
 ﻿Public Class Printer
 
+    ''' <summary>
+    ''' The name of the printer
+    ''' </summary>
+    ''' <value>The name of the printer</value>
+    ''' <returns>String, the name of the printer</returns>
+    ''' <remarks></remarks>
     Public Property Name As String
+
+    ''' <summary>
+    ''' Whether the printer is enabled or not
+    ''' </summary>
+    ''' <value>Whether the printer is enabled or not</value>
+    ''' <returns>True, if the printer is enabled, otherwise false</returns>
+    ''' <remarks></remarks>
     Public Property Enabled As Boolean
 
+    ''' <summary>
+    ''' Constructor
+    ''' </summary>
+    ''' <param name="pname">The name of the printer</param>
+    ''' <param name="penabled">Whether the printer is enabled or not</param>
+    ''' <remarks></remarks>
     Public Sub New(ByVal pname As String, Optional penabled As Boolean = False)
         Name = pname
         Enabled = penabled
