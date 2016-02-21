a14f06dd41f9f3b7b453546ba618fb118b2cca9c
Ryan O'Day <romanguy722@gmail.com>
2016-02-21 15:48:06 -0500

    Comment ProcessManager, specify string for function

--- src/Aire.API/ProcessManager.vb
+++ src/Aire.API/ProcessManager.vb
@@ -1,13 +1,23 @@
 ﻿Namespace Sys
 
     Public Class Process
-
+        ''' <summary>
+        ''' Starts a process
+        ''' </summary>
+        ''' <param name="proc">The process to start</param>
+        ''' <remarks></remarks>
         Public Shared Sub Start(ByVal proc As String)
             System.Diagnostics.Process.Start(proc)
         End Sub
 
 #Region "ExecuteCommand"
 
+        ''' <summary>
+        ''' Executes a command
+        ''' </summary>
+        ''' <param name="file">The command to execute</param>
+        ''' <param name="arguments">The arguments for the command</param>
+        ''' <remarks></remarks>
         Public Shared Sub ExecuteCommand(ByVal file As String, Optional ByVal arguments As String = "")
             Try
                 Dim proc As System.Diagnostics.Process = New System.Diagnostics.Process()
@@ -22,6 +32,13 @@
             End Try
         End Sub
 
+        ''' <summary>
+        ''' Executes a command and captures the output
+        ''' </summary>
+        ''' <param name="file">The command to execute</param>
+        ''' <param name="arguments">The arguments for the command</param>
+        ''' <returns>String, the output of the command</returns>
+        ''' <remarks></remarks>
         Public Shared Function ExecuteCommandWithOutput(ByVal file As String, Optional ByVal arguments As String = "") As String
             Try
                 Dim ret As String = ""
@@ -46,6 +63,11 @@
 
 #Region "ExecuteScript"
 
+        ''' <summary>
+        ''' Executes a bash script
+        ''' </summary>
+        ''' <param name="script">The location of the script</param>
+        ''' <remarks></remarks>
         Public Shared Sub ExecuteScript(ByVal script As String)
             Try
                 Dim proc As New System.Diagnostics.Process()
@@ -59,7 +81,13 @@
             End Try
         End Sub
 
-        Public Shared Function ExecuteScriptWithOutput(ByVal script As String)
+        ''' <summary>
+        ''' Executes a bash script and captures the output
+        ''' </summary>
+        ''' <param name="script">The location of the script</param>
+        ''' <returns>String, the output of the script</returns>
+        ''' <remarks></remarks>
+        Public Shared Function ExecuteScriptWithOutput(ByVal script As String) As String
             Try
                 Dim ret As String = ""
                 Dim proc As New System.Diagnostics.Process()
