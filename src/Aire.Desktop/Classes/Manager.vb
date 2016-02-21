5922c9c173ea3360cb68bdd41b35df8c72198bda
Ryan O'Day <romanguy722@gmail.com>
2016-02-21 16:30:33 -0500

    Comment Manager

--- src/Aire.Desktop/Classes/Manager.vb
+++ src/Aire.Desktop/Classes/Manager.vb
@@ -2,7 +2,20 @@
 
 #Region "Properties/Variables"
 
+    ''' <summary>
+    ''' The list of desktop sessions currently running
+    ''' </summary>
+    ''' <value>The list of desktop sessions currently running</value>
+    ''' <returns>List of UserDesktopSession, the list of desktop sessions currently running</returns>
+    ''' <remarks></remarks>
     Public Shared Property DesktopSessions As New List(Of UserDesktopSession)
+
+    ''' <summary>
+    ''' The currently active user desktop session
+    ''' </summary>
+    ''' <value>The currently active user desktop session</value>
+    ''' <returns>UserDesktopSession, the currently active user desktop session</returns>
+    ''' <remarks></remarks>
     Public Shared Property ActiveSession As UserDesktopSession
 
 #End Region
@@ -21,6 +34,12 @@
         ActiveSession.LoadSession()
     End Sub
 
+    ''' <summary>
+    ''' Loads a desktop session for a user
+    ''' </summary>
+    ''' <param name="user">The user to load a desktop session for</param>
+    ''' <param name="pass">The password of the user</param>
+    ''' <remarks>If a desktop session does not exist for the user, a new one is created</remarks>
     Public Shared Sub LoadDesktopSession(ByVal user As String, ByVal pass As String)
         If Aire.API.User.Users.Actions.MatchPasswordForUser(user, pass) Then
             If Aire.API.User.Users.Info.GetActiveUsers().Contains(user) Then
