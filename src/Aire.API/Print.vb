ba6d4ae329a4f6e8d90c534a7ef10d2673ceec74
Ryan O'Day <romanguy722@gmail.com>
2016-02-21 15:45:18 -0500

    Comment Print

--- src/Aire.API/Print.vb
+++ src/Aire.API/Print.vb
@@ -2,11 +2,21 @@
 
     Public Class Info
 
+        ''' <summary>
+        ''' Gets the default printer
+        ''' </summary>
+        ''' <returns>Printer, the default printer</returns>
+        ''' <remarks></remarks>
         Public Shared Function GetDefaultPrinter() As Printer
             Dim bb() As String = Sys.Process.ExecuteCommandWithOutput("lpstat", "-d").Split(" ")
             Return New Printer(bb(3))
         End Function
 
+        ''' <summary>
+        ''' Gets a list of available printers
+        ''' </summary>
+        ''' <returns>List of Printers, the available printers</returns>
+        ''' <remarks></remarks>
         Public Shared Function GetPrinters() As List(Of Printer)
             Dim ret As New List(Of Printer)
             Dim bb() As String = Sys.Process.ExecuteCommandWithOutput("lpstat", "-p").Split(Environment.NewLine)
@@ -23,10 +33,22 @@
 
     Public Class Actions
 
+        ''' <summary>
+        ''' Sets the default printer
+        ''' </summary>
+        ''' <param name="dprint">The printer to set as the default printer</param>
+        ''' <remarks></remarks>
         Public Shared Sub SetDefaultPrinter(ByVal dprint As Printer)
             Sys.Process.ExecuteCommand("lpoptions", "-d " & dprint.Name)
         End Sub
 
+        ''' <summary>
+        ''' Prints specified text
+        ''' </summary>
+        ''' <param name="txt">The text to print</param>
+        ''' <param name="printer">The printer to print to</param>
+        ''' <param name="printsettings">The PrinterSettings to use</param>
+        ''' <remarks></remarks>
         Public Shared Sub PrintText(ByVal txt As String, Optional ByVal printer As Printer = Nothing, Optional ByVal printsettings As PrinterSettings = Nothing)
             Dim file As String = Aire.DataManager.DataPaths.GetSysDataLocation & "/Temp/print.txt"
             My.Computer.FileSystem.WriteAllText(file, txt, False)
@@ -43,6 +65,13 @@
             My.Computer.FileSystem.WriteAllText(file, "", False)
         End Sub
 
+        ''' <summary>
+        ''' Prints a file
+        ''' </summary>
+        ''' <param name="file">The file to print</param>
+        ''' <param name="printer">The printer to print to</param>
+        ''' <param name="printsettings">The PrinterSettings to use</param>
+        ''' <remarks></remarks>
         Public Shared Sub PrintFile(ByVal file As String, Optional ByVal printer As Printer = Nothing, Optional ByVal printsettings As PrinterSettings = Nothing)
             Dim pstr As String = ""
             If printer.Name <> "" Then
@@ -56,10 +85,18 @@
             Sys.Process.ExecuteCommand("lp", pstr)
         End Sub
 
+        ''' <summary>
+        ''' Removes the last print job
+        ''' </summary>
+        ''' <remarks></remarks>
         Public Shared Sub RemoveLastPrintJob()
             Sys.Process.ExecuteCommand("lprm")
         End Sub
 
+        ''' <summary>
+        ''' Cancels all print jobs
+        ''' </summary>
+        ''' <remarks></remarks>
         Public Shared Sub CancelAllPrintJobs()
             Sys.Process.ExecuteCommand("lpq", "cancel -a")
         End Sub
