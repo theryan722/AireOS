5dd217e3bb9fa0fc50696a3847eab4263ea4d4b3
Ryan O'Day <romanguy722@gmail.com>
2016-02-21 15:36:18 -0500

    Comment FormPosition

--- src/Aire.API/FormPosition.vb
+++ src/Aire.API/FormPosition.vb
@@ -3,6 +3,12 @@ Imports System.Drawing
 
 Public Class FormPosition
 
+    ''' <summary>
+    ''' Centers the form either in the center of the screen, or to a parent
+    ''' </summary>
+    ''' <param name="frm">The form to center</param>
+    ''' <param name="parent">The parent to which the form will be centered in</param>
+    ''' <remarks></remarks>
     Public Shared Sub CenterForm(ByVal frm As Form, Optional ByVal parent As Form = Nothing)
         Dim r As Rectangle
         If parent IsNot Nothing Then
