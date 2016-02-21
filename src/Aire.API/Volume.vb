7af8c355b380704332f6bc2261cd9c158909243f
Ryan O'Day <romanguy722@gmail.com>
2016-02-21 16:11:10 -0500

    Comment Volume

--- src/Aire.API/Volume.vb
+++ src/Aire.API/Volume.vb
@@ -10,6 +10,11 @@
 
 #End Region
 
+        ''' <summary>
+        ''' Increases the volume
+        ''' </summary>
+        ''' <param name="amount">The amount to increase the volume by</param>
+        ''' <remarks></remarks>
         Public Shared Sub Increase(ByVal amount As Integer)
             Try
                 Sys.Process.ExecuteCommand("pactl", "-- set-sink-volume 0 +" & Math.Min(amount, 100) & "%")
@@ -19,6 +24,11 @@
             End Try
         End Sub
 
+        ''' <summary>
+        ''' Decreases the volume
+        ''' </summary>
+        ''' <param name="amount">The amount to decrease the volume by</param>
+        ''' <remarks></remarks>
         Public Shared Sub Decrease(ByVal amount As Integer)
             Try
                 Sys.Process.ExecuteCommand("pactl", "-- set-sink-volume 0 -" & Math.Min(amount, 100) & "%")
@@ -28,6 +38,10 @@
             End Try
         End Sub
 
+        ''' <summary>
+        ''' Mutes the volume
+        ''' </summary>
+        ''' <remarks></remarks>
         Public Shared Sub Mute()
             Try
                 Sys.Process.ExecuteCommand("pactl", "-- set-sink-mute 0 1")
@@ -38,6 +52,10 @@
             End Try
         End Sub
 
+        ''' <summary>
+        ''' Unmutes the volume
+        ''' </summary>
+        ''' <remarks></remarks>
         Public Shared Sub Unmute()
             Try
                 Sys.Process.ExecuteCommand("pactl", "-- set-sink-mute 0 0")
@@ -48,6 +66,10 @@
             End Try
         End Sub
 
+        ''' <summary>
+        ''' Toggles whether the volume is muted or not
+        ''' </summary>
+        ''' <remarks></remarks>
         Public Shared Sub ToggleMute()
             Try
                 Sys.Process.ExecuteCommand("pactl", "-- set-sink-mute 0 toggle")
@@ -58,6 +80,11 @@
             End Try
         End Sub
 
+        ''' <summary>
+        ''' Gets the volume
+        ''' </summary>
+        ''' <returns>Integer, the volume</returns>
+        ''' <remarks></remarks>
         Public Shared Function GetVolume() As Integer
             Try
                 Dim temp As String = Sys.Process.ExecuteScriptWithOutput(Aire.DataManager.DataPaths.GetSysDataLocation & "/Scripts/getvolume.sh")
@@ -69,6 +96,11 @@
             End Try
         End Function
 
+        ''' <summary>
+        ''' Sets the volume
+        ''' </summary>
+        ''' <param name="vol">The level to set the volume to</param>
+        ''' <remarks></remarks>
         Public Shared Sub SetVolume(ByVal vol As Integer)
             Try
                 Sys.Process.ExecuteCommand("pactl", "-- set-sink-volume 0 " & Math.Min(vol, 100) & "%")
@@ -77,10 +109,19 @@
             End Try
         End Sub
 
+        ''' <summary>
+        ''' Get whether the volume is muted or not
+        ''' </summary>
+        ''' <returns>True, if the volume is muted, otherwise false</returns>
+        ''' <remarks></remarks>
         Public Shared Function GetIfMuted() As Boolean
                 Return muted
         End Function
 
+        ''' <summary>
+        ''' Displays the system volume control
+        ''' </summary>
+        ''' <remarks></remarks>
         Public Shared Sub DisplayVolumeControl()
             If Not dlgVolumeControl.Showing Then
                 volcont = New dlgVolumeControl
@@ -88,6 +129,10 @@
             End If
         End Sub
 
+        ''' <summary>
+        ''' Displays the system volume status control
+        ''' </summary>
+        ''' <remarks></remarks>
         Public Shared Sub DisplayVolumeStatus()
             If dlgVolumeStatus.Showing Then
                 volstat.UpdateVolume(GetVolume())
