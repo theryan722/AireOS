25f845d9c77d617ae374231cdee1df8f1f2f41a2
Ryan O'Day <romanguy722@gmail.com>
2016-02-21 12:59:55 -0500

    Comment PermissionCertificate()

--- src/Aire.API/Dialogs/Permission/PermissionCertificate.vb
+++ src/Aire.API/Dialogs/Permission/PermissionCertificate.vb
@@ -4,10 +4,22 @@
     Public Property GrantedBy As String
     Public Property GrantedByPermissionLevel As Integer
 
+    ''' <summary>
+    ''' Constructor
+    ''' </summary>
+    ''' <param name="grant">Whether permission was granted or not</param>
+    ''' <remarks></remarks>
     Public Sub New(ByVal grant As Boolean)
         Granted = grant
     End Sub
 
+    ''' <summary>
+    ''' Constructor
+    ''' </summary>
+    ''' <param name="grant">Whether permission was granted or not</param>
+    ''' <param name="grantby">The user that granted permission</param>
+    ''' <param name="grantbypermlevel">The permission level of the user that granted permission</param>
+    ''' <remarks></remarks>
     Public Sub New(ByVal grant As Boolean, ByVal grantby As String, ByVal grantbypermlevel As Integer)
         Granted = grant
         GrantedBy = grantby
