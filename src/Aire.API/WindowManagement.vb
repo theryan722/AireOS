f2a7c410cf96e597622a82034d86405814e08de0
Ryan O'Day <romanguy722@gmail.com>
2016-02-21 16:16:41 -0500

    Comment WindowManagement

--- src/Aire.API/WindowManagement.vb
+++ src/Aire.API/WindowManagement.vb
@@ -23,6 +23,11 @@
 
 #End Region
 
+            ''' <summary>
+            ''' Gets a list of window information, unfiltered
+            ''' </summary>
+            ''' <returns>List of string, information for all windows, unfiltered</returns>
+            ''' <remarks></remarks>
             Public Shared Function GetAllWindowsUnfiltered() As List(Of String)
                 Dim ret As New List(Of String)
                 Dim tname As String
@@ -36,6 +41,11 @@
                 Return ret
             End Function
 
+            ''' <summary>
+            ''' Gets a list of running windows
+            ''' </summary>
+            ''' <returns>List of string, the handles of running windows</returns>
+            ''' <remarks></remarks>
             Public Shared Function GetRunningWindows() As List(Of String)
                 Dim ret As New List(Of String)
                 Dim tname As String
@@ -51,14 +61,32 @@
                 Return ret
             End Function
 
+            ''' <summary>
+            ''' Gets the title of a window
+            ''' </summary>
+            ''' <param name="win">The window handle</param>
+            ''' <returns>String, the title of the window</returns>
+            ''' <remarks></remarks>
             Public Shared Function GetName(ByVal win As String) As String
                 Return Sys.Process.ExecuteCommandWithOutput("xdotool", "getwindowname " & win)
             End Function
 
+            ''' <summary>
+            ''' Checks if a window is closed
+            ''' </summary>
+            ''' <param name="win">The window handle to check</param>
+            ''' <returns>True, if the window is closed, otherwise false</returns>
+            ''' <remarks></remarks>
             Public Shared Function GetIfClosed(ByVal win As String) As Boolean
                 Return Sys.Process.ExecuteCommandWithOutput("xdotool", "getwindowpid " & win).ToLower.Contains("error")
             End Function
 
+            ''' <summary>
+            ''' Gets a window handle by its title/name
+            ''' </summary>
+            ''' <param name="fname">The window title</param>
+            ''' <returns>String, the window handle</returns>
+            ''' <remarks></remarks>
             Public Shared Function GetWindowByName(ByVal fname As String) As String
                 Dim ret As String = ""
                 For Each item As String In Aire.API.Sys.Window.Info.GetRunningWindows
@@ -74,14 +102,30 @@
 
         Public Class Actions
 
+            ''' <summary>
+            ''' Set a window to be always in the back
+            ''' </summary>
+            ''' <param name="win">The handle of the window</param>
+            ''' <remarks></remarks>
             Public Shared Sub SetAlwaysInBack(ByVal win As String)
                 Sys.Process.ExecuteCommand("wmctrl", "-i -r " & win & " -b add,below")
             End Sub
 
+            ''' <summary>
+            ''' Minimizes a window
+            ''' </summary>
+            ''' <param name="win">The handle of the window</param>
+            ''' <remarks></remarks>
             Public Shared Sub Minimize(ByVal win As String)
                 Sys.Process.ExecuteCommand("xdotool", "windowminimize " & win)
             End Sub
 
+            ''' <summary>
+            ''' Sets a window's windowstate to normal
+            ''' </summary>
+            ''' <param name="win">The handle of the window</param>
+            ''' <param name="activatewin">Whether or not to activate the window</param>
+            ''' <remarks></remarks>
             Public Shared Sub Normal(ByVal win As String, Optional ByVal activatewin As Boolean = True)
                 If activatewin Then
                     Activate(win)
@@ -89,6 +133,12 @@
                 Sys.Process.ExecuteCommand("wmctrl", "-i -r " & win & " -b remove,maximized_vert,maximized_horz")
             End Sub
 
+            ''' <summary>
+            ''' Maximizes a window
+            ''' </summary>
+            ''' <param name="win">The handle of the window</param>
+            ''' <param name="activatewin">Whether or not to activate the window</param>
+            ''' <remarks></remarks>
             Public Shared Sub Maximize(ByVal win As String, Optional ByVal activatewin As Boolean = True)
                 If activatewin Then
                     Activate(win)
@@ -96,6 +146,12 @@
                 Sys.Process.ExecuteCommand("wmctrl", "-i -r " & win & " -b add,maximized_vert,maximized_horz")
             End Sub
 
+            ''' <summary>
+            ''' Toggles whether a window is maximized or normal
+            ''' </summary>
+            ''' <param name="win">The handle of the window</param>
+            ''' <param name="activatewin">Whether or not to activate the window</param>
+            ''' <remarks></remarks>
             Public Shared Sub ToggleMaximizeNormal(ByVal win As String, Optional ByVal activatewin As Boolean = True)
                 If activatewin Then
                     Activate(win)
@@ -103,18 +159,38 @@
                 Sys.Process.ExecuteCommand("wmctrl", "-i -r " & win & " -b toggle,maximized_vert,maximized_horz")
             End Sub
 
+            ''' <summary>
+            ''' Closes a window
+            ''' </summary>
+            ''' <param name="win">The handle of the window</param>
+            ''' <remarks></remarks>
             Public Shared Sub Kill(ByVal win As String)
                 Sys.Process.ExecuteCommand("xdotool", "windowkill " & win)
             End Sub
 
+            ''' <summary>
+            ''' Activates (gives focus to) a window
+            ''' </summary>
+            ''' <param name="win">The handle of the window</param>
+            ''' <remarks></remarks>
             Public Shared Sub Activate(ByVal win As String)
                 Sys.Process.ExecuteCommand("xdotool", "windowactivate " & win)
             End Sub
 
+            ''' <summary>
+            ''' Hides a window
+            ''' </summary>
+            ''' <param name="win">The handle of the window</param>
+            ''' <remarks></remarks>
             Public Shared Sub Hide(ByVal win As String)
                 Sys.Process.ExecuteCommand("xdotool", "windowunmap " & win)
             End Sub
 
+            ''' <summary>
+            ''' Shows a window
+            ''' </summary>
+            ''' <param name="win">The handle of the window</param>
+            ''' <remarks></remarks>
             Public Shared Sub Show(ByVal win As String)
                 Sys.Process.ExecuteCommand("xdotool", "windowmap " & win)
             End Sub
