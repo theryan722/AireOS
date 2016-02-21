9b559194de0820480716ddd2e0c166d8c4556b5c
Ryan O'Day <romanguy722@gmail.com>
2016-02-21 16:24:38 -0500

    Comment TempData

--- src/Aire.DataManager/TempData.vb
+++ src/Aire.DataManager/TempData.vb
@@ -1,5 +1,11 @@
 ﻿Public Class TempData
 
+    ''' <summary>
+    ''' Gets the currently logged in user
+    ''' </summary>
+    ''' <value>The currently logged in user</value>
+    ''' <returns>String, the username of the currently logged in user</returns>
+    ''' <remarks></remarks>
     Public Shared Property LoggedInUser As String
 
 End Class
