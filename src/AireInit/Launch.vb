878677dc54196abfb6fa0c1ed4049470fff83af8
Ryan O'Day <romanguy722@gmail.com>
2016-02-21 16:27:39 -0500

    Add lines

--- src/AireInit/Launch.vb
+++ src/AireInit/Launch.vb
@@ -1,6 +1,8 @@
 ﻿Module Launch
+
     Private hostloc As String = Aire.API.Compatibility.FixPath(AppDomain.CurrentDomain.BaseDirectory & "/Aire.Host.exe")
     Private setuploc As String = Aire.API.Compatibility.FixPath(AppDomain.CurrentDomain.BaseDirectory & "/Aire.FirstTimeSetup.exe")
+
     'Initializes Aire OS
     Sub Main()
         Console.Title = "Aire OS Initialization"
