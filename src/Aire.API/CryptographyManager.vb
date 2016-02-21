5925428a8088a1fcf306f1b6662678e87ae310e0
Ryan O'Day <romanguy722@gmail.com>
2016-02-21 15:32:34 -0500

    Comment CryptographyManager

--- src/Aire.API/CryptographyManager.vb
+++ src/Aire.API/CryptographyManager.vb
@@ -4,18 +4,43 @@ Namespace Cryptography
 
     Public Class Passwords
 
+        ''' <summary>
+        ''' Hashes a password string
+        ''' </summary>
+        ''' <param name="pass">The password to hash</param>
+        ''' <returns>String, the hashed password</returns>
+        ''' <remarks></remarks>
         Public Shared Function HashPassword(ByVal pass As String) As String
             Return HashPassword(pass, GenerateSalt())
         End Function
 
+        ''' <summary>
+        ''' Hashes a password string
+        ''' </summary>
+        ''' <param name="pass">The password to hash</param>
+        ''' <param name="salt">The salt to hash the password with</param>
+        ''' <returns>String, the hashed password</returns>
+        ''' <remarks></remarks>
         Public Shared Function HashPassword(ByVal pass As String, ByVal salt As String) As String
             Return BCrypt.Net.BCrypt.HashPassword(pass, salt)
         End Function
 
+        ''' <summary>
+        ''' Generates a salt
+        ''' </summary>
+        ''' <returns>String, the generated salt</returns>
+        ''' <remarks></remarks>
         Public Shared Function GenerateSalt() As String
             Return BCrypt.Net.BCrypt.GenerateSalt()
         End Function
 
+        ''' <summary>
+        ''' Compares a password string to a hashed password and checks if they match
+        ''' </summary>
+        ''' <param name="pass">The password in plaintext</param>
+        ''' <param name="hashedpass">The hashed password</param>
+        ''' <returns>True, if the passwords match, otherwise false</returns>
+        ''' <remarks></remarks>
         Public Shared Function CheckPassword(ByVal pass As String, ByVal hashedpass As String) As Boolean
             Return Verify(pass, hashedpass)
         End Function
@@ -24,6 +49,12 @@ Namespace Cryptography
 
     Public Class Text
 
+        ''' <summary>
+        ''' Hashes a string
+        ''' </summary>
+        ''' <param name="text">The string to hash</param>
+        ''' <returns>The hashed string</returns>
+        ''' <remarks></remarks>
         Public Shared Function HashText(ByVal text As String) As String
             Return HashString(text)
         End Function
