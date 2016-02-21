7d53c368769035c79c01854a43dda66276045022
Ryan O'Day <romanguy722@gmail.com>
2016-02-21 15:34:52 -0500

    Comment Events

--- src/Aire.API/Events.vb
+++ src/Aire.API/Events.vb
@@ -15,14 +15,36 @@
 
 #Region "Events"
 
+        ''' <summary>
+        ''' Raised when the system power state is changed, i.e. sleep, shutdown, restart, etc.
+        ''' </summary>
+        ''' <param name="change">The type of power state change</param>
+        ''' <remarks></remarks>
         Public Shared Event SystemPowerStateChanged(ByVal change As PowerState)
+
+        ''' <summary>
+        ''' Raised when the volume is changed
+        ''' </summary>
+        ''' <param name="volume">The volume</param>
+        ''' <remarks></remarks>
         Public Shared Event VolumeChanged(ByVal volume As Integer)
+
+        ''' <summary>
+        ''' Raised when the battery percentage is changed
+        ''' </summary>
+        ''' <param name="percent">The battery percentage</param>
+        ''' <remarks></remarks>
         Public Shared Event BatteryPercentChanged(ByVal percent As Integer)
 
 #End Region
 
 #Region "Raise Event Methods"
 
+        ''' <summary>
+        ''' Raises the SystemPowerStateChanged event
+        ''' </summary>
+        ''' <param name="change">The powerstate change</param>
+        ''' <remarks></remarks>
         Public Shared Sub Raise_SystemPowerStateChanged(ByVal change As PowerState)
             RaiseEvent SystemPowerStateChanged(change)
             Select Case change
@@ -37,11 +59,21 @@
             End Select
         End Sub
 
+        ''' <summary>
+        ''' Raises the VolumeChanged event
+        ''' </summary>
+        ''' <param name="volume">The volume</param>
+        ''' <remarks></remarks>
         Public Shared Sub Raise_VolumeChanged(ByVal volume As Integer)
             RaiseEvent VolumeChanged(volume)
             Audio.Volume.DisplayVolumeStatus()
         End Sub
 
+        ''' <summary>
+        ''' Raises the BatteryPercentChanged event
+        ''' </summary>
+        ''' <param name="percent">The battery level</param>
+        ''' <remarks></remarks>
         Public Shared Sub Raise_BatteryPercentChanged(ByVal percent As Integer)
             RaiseEvent BatteryPercentChanged(percent)
         End Sub
