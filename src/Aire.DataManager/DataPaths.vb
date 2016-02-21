19d30f5618d261376d0ff2447c63b65e23f3fea8
Ryan O'Day <romanguy722@gmail.com>
2016-02-21 16:23:03 -0500

    Comment DataPaths

--- src/Aire.DataManager/DataPaths.vb
+++ src/Aire.DataManager/DataPaths.vb
@@ -1,13 +1,28 @@
 ﻿Public Class DataPaths
 
+    ''' <summary>
+    ''' Gets the location of the operating system
+    ''' </summary>
+    ''' <returns>String, the root path of the OS</returns>
+    ''' <remarks></remarks>
     Public Shared Function GetOSLocation() As String
         Return AppDomain.CurrentDomain.BaseDirectory.TrimEnd("/")
     End Function
 
+    ''' <summary>
+    ''' Gets the system data location
+    ''' </summary>
+    ''' <returns>String, the path of the system data folder</returns>
+    ''' <remarks></remarks>
     Public Shared Function GetSysDataLocation() As String
         Return AppDomain.CurrentDomain.BaseDirectory & "SysData"
     End Function
 
+    ''' <summary>
+    ''' Gets the system application list location
+    ''' </summary>
+    ''' <returns>String, the location of the system application list</returns>
+    ''' <remarks></remarks>
     Public Shared Function GetSystemApplicationList() As String
         Return AppDomain.CurrentDomain.BaseDirectory & "SysData/sysapplist.txt"
     End Function
