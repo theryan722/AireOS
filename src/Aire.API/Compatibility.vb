50be9251638d0594cde55ede41b54ce92cebe18c
Ryan O'Day <romanguy722@gmail.com>
2016-02-21 15:29:56 -0500

    Comment Compatibility

--- src/Aire.API/Compatibility.vb
+++ src/Aire.API/Compatibility.vb
@@ -1,5 +1,11 @@
 ﻿Public Class Compatibility
 
+    ''' <summary>
+    ''' Checks and fixes improper slashes
+    ''' </summary>
+    ''' <param name="path">The path to fix</param>
+    ''' <returns>String, the fixed path</returns>
+    ''' <remarks></remarks>
     Public Shared Function FixPath(ByVal path As String) As String
         Return path.Replace("\", "/").Replace("//", "/")
     End Function
