64c6adfecaef8e83d720ea59466ebfbede3da8d7
Ryan O'Day <romanguy722@gmail.com>
2016-03-07 15:27:05 -0500

    Update Manager

--- src/Aire.SMManager/Manager.vb
+++ src/Aire.SMManager/Manager.vb
@@ -1,47 +1,31 @@
-﻿Public Class Manager
+﻿Imports System.IO
+
+Public Class Manager
 
 #Region "Public"
 
     Public Shared Sub UpdateFile(ByVal file As String, ByVal txt As String)
-
+        Dim outStream = New FileStream(file, FileMode.Open, FileAccess.Write, FileShare.ReadWrite)
+        Dim arr() As Byte = System.Text.Encoding.ASCII.GetBytes(txt)
+        outStream.Write(System.Text.Encoding.ASCII.GetBytes(txt), 0, arr.Count)
     End Sub
 
     Public Shared Function ReadFile(ByVal file As String) As String
-
-    End Function
-
-#End Region
-
-#Region "Helper"
-
-    Private Shared Sub ExecuteScript(ByVal script As String)
-        Try
-            Dim proc As New System.Diagnostics.Process()
-            proc.StartInfo.FileName = "/usr/bin/python"
-            proc.StartInfo.Arguments = script
-            proc.StartInfo.UseShellExecute = False
-            proc.StartInfo.RedirectStandardOutput = True
-            proc.Start()
-        Catch
-        End Try
-    End Sub
-
-    Private Shared Function ExecuteScriptWithOutput(ByVal script As String) As String
-        Try
-            Dim ret As String = ""
-            Dim proc As New System.Diagnostics.Process()
-            proc.StartInfo.FileName = "/usr/bin/python"
-            proc.StartInfo.Arguments = script
-            proc.StartInfo.UseShellExecute = False
-            proc.StartInfo.RedirectStandardOutput = True
-            proc.Start()
-            ret = proc.StandardOutput.ReadToEnd()
-            proc.WaitForExit()
-            proc.Close()
-            Return ret
-        Catch
-            Return Nothing
-        End Try
+        Dim inStream = New FileStream(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
+        Dim bytes() As Byte = New Byte((inStream.Length) - 1) {}
+        Dim numBytesToRead As Integer = CType(inStream.Length, Integer)
+        Dim numBytesRead As Integer = 0
+        While (numBytesToRead > 0)
+            Dim n As Integer = inStream.Read(bytes, numBytesRead, _
+                numBytesToRead)
+            If (n = 0) Then
+                Exit While
+            End If
+            numBytesRead = (numBytesRead + n)
+            numBytesToRead = (numBytesToRead - n)
+        End While
+        numBytesToRead = bytes.Length
+        Return System.Text.ASCIIEncoding.ASCII.GetString(bytes)
     End Function
 
 #End Region
