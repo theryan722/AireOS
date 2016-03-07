3b7d7df2d968bc8505bfcd1f89d67db41519ea8f
Ryan O'Day <romanguy722@gmail.com>
2016-03-07 14:14:11 -0500

    Fix type

--- src/Scripts/smemory.py
+++ src/Scripts/smemory.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 
 import sys
-
+# 1: CREATE, UPDATE, READ
+# 2: File Location
+# 3: Text
 if len(sys.argv) > 1:
-	if sys.argv(1) == "CREATE":
-	elif sys.argv(1) == "UPDATE"
-	elif sys.argv(1) == "READ"
+	if sys.argv[1] == "CREATE":
+		print "Create"
+	elif sys.argv[1] == "UPDATE":
+		print "Update"
+	elif sys.argv[1] == "READ":
+		print "Read"
