53402213d358726df526ee41f0206a08ffab21c3
Ryan O'Day <romanguy722@gmail.com>
2016-03-07 15:04:48 -0500

    Copy smemory to compiled, remove CreateFile()

--- src/Aire.SMManager/Manager.vb
+++ src/Aire.SMManager/Manager.vb
@@ -2,10 +2,6 @@
 
 #Region "Public"
 
-    Public Shared Sub CreateFile(ByVal file As String, ByVal key As String, ByVal obj As Object, ByVal buffer As Integer)
-
-    End Sub
-
     Public Shared Sub UpdateFile(ByVal key As String, ByVal obj As Object)
 
     End Sub
