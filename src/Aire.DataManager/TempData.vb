c63d5faf1666101b8de85e4de14530d310895f0f
Ryan O'Day <romanguy722@gmail.com>
2016-03-07 15:48:51 -0500

    LoggedInUser property now uses shared data

--- src/Aire.DataManager/TempData.vb
+++ src/Aire.DataManager/TempData.vb
@@ -10,5 +10,12 @@ Public Class TempData
     ''' <returns>String, the username of the currently logged in user</returns>
     ''' <remarks></remarks>
     Public Shared Property LoggedInUser As String
+        Get
+            Return Aire.SMManager.Manager.ReadFile(DataPaths.GetSysDataLocation & "/Temp/currentuser.txt")
+        End Get
+        Set(value As String)
+            Aire.SMManager.Manager.UpdateFile(DataPaths.GetSysDataLocation & "/Temp/currentuser.txt", value)
+        End Set
+    End Property
 
 End Class
