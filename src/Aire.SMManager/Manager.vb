2afd7afc3479f8df165ec2f0593d4823600fa3c4
Ryan O'Day <romanguy722@gmail.com>
2016-03-06 22:14:16 -0500

    ExecuteScript(), ExecuteScriptWithOutput()

--- src/Aire.SMManager/Manager.vb
+++ src/Aire.SMManager/Manager.vb
@@ -12,4 +12,34 @@
 
     End Function
 
+    Private Shared Sub ExecuteScript(ByVal script As String)
+        Try
+            Dim proc As New System.Diagnostics.Process()
+            proc.StartInfo.FileName = "/bin/bash"
+            proc.StartInfo.Arguments = script
+            proc.StartInfo.UseShellExecute = False
+            proc.StartInfo.RedirectStandardOutput = True
+            proc.Start()
+        Catch
+        End Try
+    End Sub
+
+    Private Shared Function ExecuteScriptWithOutput(ByVal script As String) As String
+        Try
+            Dim ret As String = ""
+            Dim proc As New System.Diagnostics.Process()
+            proc.StartInfo.FileName = "/usr/bin/python"
+            proc.StartInfo.Arguments = script
+            proc.StartInfo.UseShellExecute = False
+            proc.StartInfo.RedirectStandardOutput = True
+            proc.Start()
+            ret = proc.StandardOutput.ReadToEnd()
+            proc.WaitForExit()
+            proc.Close()
+            Return ret
+        Catch
+            Return Nothing
+        End Try
+    End Function
+
 End Class
