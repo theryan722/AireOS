a06afad18abde784e7cb4067ac45fac4a5cbae60
Ryan O'Day <romanguy722@gmail.com>
2016-03-06 20:53:33 -0500

    Manager code outline

--- src/Aire.SMManager/Manager.vb
+++ src/Aire.SMManager/Manager.vb
@@ -1,3 +1,15 @@
 ﻿Public Class Manager
 
+    Public Shared Sub CreateFile(ByVal file As String, ByVal buffer As Integer)
+
+    End Sub
+
+    Public Shared Sub UpdateFile(ByVal file As String, ByVal txt As String)
+
+    End Sub
+
+    Public Shared Function ReadFile(ByVal file As String)
+
+    End Function
+
 End Class
