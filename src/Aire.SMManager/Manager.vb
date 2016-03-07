737b722b0afb6a4b1f17f4ac8cbf871976010d8d
Ryan O'Day <romanguy722@gmail.com>
2016-03-06 20:56:25 -0500

    Update Manager

--- src/Aire.SMManager/Manager.vb
+++ src/Aire.SMManager/Manager.vb
@@ -1,14 +1,14 @@
 ﻿Public Class Manager
 
-    Public Shared Sub CreateFile(ByVal file As String, ByVal buffer As Integer)
+    Public Shared Sub CreateFile(ByVal file As String, ByVal key As String, ByVal obj As Object, ByVal buffer As Integer)
 
     End Sub
 
-    Public Shared Sub UpdateFile(ByVal file As String, ByVal txt As String)
+    Public Shared Sub UpdateFile(ByVal key As String, ByVal obj As Object)
 
     End Sub
 
-    Public Shared Function ReadFile(ByVal file As String)
+    Public Shared Function ReadFile(ByVal key As String) As Object
 
     End Function
 
