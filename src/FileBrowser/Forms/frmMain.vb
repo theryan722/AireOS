5494619288ed1a788229c1635c8b0f41ab2552fd
Ryan O'Day <romanguy722@gmail.com>
2016-03-07 20:07:36 -0500

    Regions

--- src/FileBrowser/Forms/frmMain.vb
+++ src/FileBrowser/Forms/frmMain.vb
@@ -1,3 +1,29 @@
 ﻿Public Class frmMain
 
+#Region "MenuStrip"
+
+
+
+#End Region
+
+#Region "Methods"
+
+
+
+#End Region
+
+#Region "ListView1"
+
+
+
+#End Region
+
+#Region "frmMain"
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+#End Region
+    
 End Class
