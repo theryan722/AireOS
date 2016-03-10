b93cb93a943af0e2e91eb4ad15854875e5e30ec3
Ryan O'Day <romanguy722@gmail.com>
2016-03-10 15:51:54 -0500

    Work on dlgProperties

--- src/FileBrowser/Forms/dlgProperties.Designer.vb
+++ src/FileBrowser/Forms/dlgProperties.Designer.vb
@@ -22,17 +22,62 @@ Partial Class dlgProperties
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.btnOk = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'btnOk
+        '
+        Me.btnOk.Dock = System.Windows.Forms.DockStyle.Right
+        Me.btnOk.Location = New System.Drawing.Point(209, 0)
+        Me.btnOk.Name = "btnOk"
+        Me.btnOk.Size = New System.Drawing.Size(75, 23)
+        Me.btnOk.TabIndex = 0
+        Me.btnOk.Text = "Ok"
+        Me.btnOk.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.btnOk)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel1.Location = New System.Drawing.Point(0, 388)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(284, 23)
+        Me.Panel1.TabIndex = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(284, 388)
+        Me.TextBox1.TabIndex = 2
         '
         'dlgProperties
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(284, 411)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Panel1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
         Me.Name = "dlgProperties"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
         Me.Text = "Properties"
+        Me.Panel1.ResumeLayout(False)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents btnOk As System.Windows.Forms.Button
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
 End Class
--- src/FileBrowser/Forms/dlgProperties.vb
+++ src/FileBrowser/Forms/dlgProperties.vb
@@ -1,10 +1,20 @@
-﻿Public Class dlgProperties
+﻿Imports System.IO
+
+Public Class dlgProperties
 
     Private fproperties As FileProperties
+    Private isfolder As Boolean = False
 
 #Region "Methods"
 
-
+    Private Sub LoadProperties()
+        TextBox1.Text = ""
+        If isfolder Then
+            TextBox1.Text = ""
+        Else
+            TextBox1.Text = "Name: " & Path.GetFileName(fproperties.GetFullPath) & vbNewLine & "Folder: " & Path.GetDirectoryName(fproperties.GetFullPath) & vbNewLine & "Full Path: " & fproperties.GetFullPath & vbNewLine & "Size: " & fproperties.GetFileSize & vbNewLine & "Created: " & fproperties.GetFileCreationDate.ToString & vbNewLine & "Modified: " & fproperties.GetFileModifiedDate.ToString & vbNewLine & "Accessed: " & fproperties.GetAccessedDate & vbNewLine & "Read Only: " & fproperties.GetIfReadOnly.ToString
+        End If
+    End Sub
 
 #End Region
 
@@ -15,10 +25,16 @@
     End Sub
 
     Private Sub dlgProperties_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.Text = System.IO.Path.GetFileName(fproperties.GetFullPath) & " Properties"
+        If fproperties.GetIfFileExists Then
+            Me.Text = System.IO.Path.GetFileName(fproperties.GetFullPath) & " Properties"
+            isfolder = False
+        Else
+            Me.Text = System.IO.Path.GetDirectoryName(fproperties.GetFullPath) & " Properties"
+            isfolder = True
+        End If
     End Sub
 
 #End Region
 
-    
+
 End Class
