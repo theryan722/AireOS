1bdc7957beadccd911a2d0a9edac4a1ddcf31046
Ryan O'Day <romanguy722@gmail.com>
2016-03-10 15:15:28 -0500

    FilePropertiesClass

---
+++ src/FileBrowser/Classes/FileProperties.vb
@@ -0,0 +1,15 @@
+﻿Public Class FileProperties
+
+    Private fpath As String
+
+#Region "Methods"
+
+
+
+#End Region
+
+    Public Sub New(ByVal path As String)
+        fpath = path
+    End Sub
+
+End Class
