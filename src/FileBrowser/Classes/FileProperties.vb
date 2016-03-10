755a3dc8887a09ad0a753e50afb5e90a5d62ef09
Ryan O'Day <romanguy722@gmail.com>
2016-03-10 15:34:01 -0500

    Fix naming issue, fix labeling

--- src/FileBrowser/Classes/FileProperties.vb
+++ src/FileBrowser/Classes/FileProperties.vb
@@ -10,7 +10,7 @@ Public Class FileProperties
         Return File.Exists(fpath)
     End Function
 
-    Public Function GetFileSize() As String
+    Public Function GetFileSizeInBytes() As String
         Return New FileInfo(fpath).Length
     End Function
 
@@ -45,19 +45,19 @@ Public Class FileProperties
             Select Case TheSize
                 Case Is >= 1099511627776
                     DoubleBytes = CDbl(TheSize / 1099511627776) 'TB
-                    Return FormatNumber(DoubleBytes, 2) & " TB"
+                    Return FormatNumber(DoubleBytes, 2) & "TB"
                 Case 1073741824 To 1099511627775
                     DoubleBytes = CDbl(TheSize / 1073741824) 'GB
-                    Return FormatNumber(DoubleBytes, 2) & " GB"
+                    Return FormatNumber(DoubleBytes, 2) & "GB"
                 Case 1048576 To 1073741823
                     DoubleBytes = CDbl(TheSize / 1048576) 'MB
-                    Return FormatNumber(DoubleBytes, 2) & " MB"
+                    Return FormatNumber(DoubleBytes, 2) & "MB"
                 Case 1024 To 1048575
                     DoubleBytes = CDbl(TheSize / 1024) 'KB
-                    Return FormatNumber(DoubleBytes, 2) & " KB"
+                    Return FormatNumber(DoubleBytes, 2) & "KB"
                 Case 0 To 1023
                     DoubleBytes = TheSize ' bytes
-                    Return FormatNumber(DoubleBytes, 2) & " bytes"
+                    Return FormatNumber(DoubleBytes, 2) & "B"
                 Case Else
                     Return ""
             End Select
