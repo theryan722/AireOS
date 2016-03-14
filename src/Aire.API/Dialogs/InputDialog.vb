630948150a6596ffb68251bcdad5f99c1f6955fd
Ryan O'Day <romanguy722@gmail.com>
2016-03-14 18:52:30 -0400

    Outline code

--- src/Aire.API/Dialogs/InputDialog.vb
+++ src/Aire.API/Dialogs/InputDialog.vb
@@ -2,7 +2,7 @@
 
 #Region "Properties"
 
-
+    Public Property Response As String
 
 #End Region
 
@@ -14,13 +14,25 @@
 
 #Region "UI"
 
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
 
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+
+    End Sub
+
+    Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
+
+    End Sub
 
 #End Region
 
 #Region "InputDialog"
 
+    Public Sub New(Optional ByVal multiline As Boolean = False, Optional ByVal title As String = "", Optional ByVal message As String = "")
 
+    End Sub
 
 #End Region
 
