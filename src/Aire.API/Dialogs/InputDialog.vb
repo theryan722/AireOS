448fbad9c29b44a9267de472fcd1692155e478f8
Ryan O'Day <romanguy722@gmail.com>
2016-03-14 18:49:37 -0400

    Regions

--- src/Aire.API/Dialogs/InputDialog.vb
+++ src/Aire.API/Dialogs/InputDialog.vb
@@ -1,5 +1,27 @@
 ﻿Public Class InputDialog
 
+#Region "Properties"
 
+
+
+#End Region
+
+#Region "Methods"
+
+
+
+#End Region
+
+#Region "UI"
+
+
+
+#End Region
+
+#Region "InputDialog"
+
+
+
+#End Region
 
 End Class
