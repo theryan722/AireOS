7fb7b9d8b0dd4e8492d203220b12dcaf589de919
Ryan O'Day <romanguy722@gmail.com>
2016-03-17 15:35:58 -0400

    About Dialog

--- src/FileBrowser/Forms/Dialogs/dlgAbout.Designer.vb
+++ src/FileBrowser/Forms/Dialogs/dlgAbout.Designer.vb
@@ -22,8 +22,52 @@ Partial Class dlgAbout
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnOk = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(269, 163)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Text = "Aire OS File Browser" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "----------------------------" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Version 1.0.0.0" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Developed by" & _
+    " Ryan O'Day" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Copyright © 2016 Ryan O'Day. All Rights Reserved." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "----------------" & _
+    "------------"
+        '
+        'btnOk
+        '
+        Me.btnOk.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.btnOk.Location = New System.Drawing.Point(0, 163)
+        Me.btnOk.Name = "btnOk"
+        Me.btnOk.Size = New System.Drawing.Size(269, 23)
+        Me.btnOk.TabIndex = 2
+        Me.btnOk.Text = "Ok"
+        Me.btnOk.UseVisualStyleBackColor = True
+        '
+        'dlgAbout
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "dlgAbout"
+        Me.ClientSize = New System.Drawing.Size(269, 186)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnOk)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "dlgAbout"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = "About"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents btnOk As System.Windows.Forms.Button
 End Class
