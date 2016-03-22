2f6d232dd8ca255bfcaa6b24cb7b8cdff9a18782
Ryan O'Day <romanguy722@gmail.com>
2016-03-22 16:41:25 -0400

    Implement btnClose

--- src/FileBrowser/Forms/Dialogs/dlgHistory.vb
+++ src/FileBrowser/Forms/Dialogs/dlgHistory.vb
@@ -21,7 +21,7 @@
     End Sub
 
     Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
-
+        Me.Close()
     End Sub
 
     Private Sub btnNavigate_Click(sender As Object, e As EventArgs) Handles btnNavigate.Click
