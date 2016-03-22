a9f78373e25d8604f677d107ef34c5b77bf872da
Ryan O'Day <romanguy722@gmail.com>
2016-03-22 16:54:22 -0400

    Outline context menu code

--- src/FileBrowser/Forms/Dialogs/dlgBookmarks.vb
+++ src/FileBrowser/Forms/Dialogs/dlgBookmarks.vb
@@ -1,3 +1,45 @@
 ﻿Public Class dlgBookmarks
 
+#Region "UI"
+
+#Region "Buttons"
+
+
+
+#End Region
+
+#Region "Context Menu Strip"
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub OpenInNewWindowToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenInNewWindowToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub RemoveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RemoveToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub AddToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AddToolStripMenuItem.Click
+
+    End Sub
+
+#End Region
+
+#End Region
+
+#Region "Methods"
+
+
+
+#End Region
+
+#Region "dlgBookmarks"
+
+
+
+#End Region
+
 End Class
